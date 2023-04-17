99e1b55f980ee4711db776854bc96ecdc8f6b319
pshu <pishu.spf@antfin.com>
2023-04-18 00:44:31 +0800

    feat: ✨ 命令行解析配置参数

--- crates/maro_cli/src/main.rs
+++ crates/maro_cli/src/main.rs
@@ -1,3 +1,47 @@
+use std::collections::HashMap;
+use std::path::{Path, PathBuf};
+use clap::{Parser, command};
+use mako_bundler::config::Config;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    entry: String,
+    #[arg(short, long, default_value = ".")]
+    root: String,
+}
+
+
+fn join_paths(current: &Path, another: &Path) -> PathBuf {
+    let joined = current.join(another);
+    joined.canonicalize().expect("Failed to canonicalize path")
+}
+
+impl Into<Config> for Cli {
+    fn into(self) -> Config {
+        let current_path = std::env::current_dir().expect("Failed to get current directory");
+        let result = join_paths(&current_path, &Path::new(self.root.as_str()));
+
+        Config {
+            entry: {
+                let mut map = HashMap::new();
+                map.insert("index".to_string(), self.entry);
+                map
+            },
+            root: result.to_string_lossy().to_string(),
+            externals: {
+                let mut map = HashMap::new();
+                map.insert("react".to_string(), "React".to_string());
+                map.insert("react-dom/client".to_string(), "ReactDOM".to_string());
+                map
+            },
+            ..Default::default()
+        }
+    }
+}
+
 fn main() {
-    mako_bundler::run();
+    let cli = Cli::parse();
+    mako_bundler::run_with_config(cli.into())
 }
