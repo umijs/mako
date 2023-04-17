c5c9d690945cef5c779a72c24db0dab5dca90038
vagusx <vagusxl@gmail.com>
2023-04-17 22:04:35 +0800

    feat: use base64 directly and remove rustc_serialize for security reason from ci report

--- crates/mako_bundler/src/utils/file.rs
+++ crates/mako_bundler/src/utils/file.rs
@@ -1,15 +1,10 @@
-use rustc_serialize::base64::{ToBase64, MIME};
+use base64::{Engine, engine, alphabet::{STANDARD}};
 use std::{
     fs,
     io::{BufRead, BufReader},
     path::Path,
     str,
 };
-
-pub fn copy_file(source_path: &str, target_path: &str) -> std::io::Result<()> {
-    fs::copy(source_path, target_path)?;
-    Ok(())
-}
 
 pub fn ext_name(path: &str) -> &str {
     Path::new(path).extension().unwrap().to_str().unwrap()
@@ -47,9 +42,15 @@ pub fn content_hash(file_path: &str) -> std::io::Result<String> {
 
 pub fn to_base64(path: &str) -> std::io::Result<String> {
     let vec = fs::read(path)?;
-    let base64 = vec.to_base64(MIME);
+	let engine = engine::GeneralPurpose::new(
+        &STANDARD,
+        engine::general_purpose::PAD,
+    );
+
+	let base64 = engine.encode(&vec);
+
     // 直接用 extension 可能处理不了 jpeg 格式的情况
-    let file_type = Path::new(path).extension().unwrap().to_str().unwrap();
+    let file_type = ext_name(path);
     Ok(format!(
         "data:image/{};base64,{}",
         file_type,
