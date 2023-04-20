407901d532c42d58b7cd0609f032f9bac739dabf
禾登 <bangjun.wbj@antgroup.com>
2023-04-20 13:47:14 +0800

    test: add testcases

--- crates/mako_bundler/tests/bundler_test.rs
+++ crates/mako_bundler/tests/bundler_test.rs
@@ -115,6 +115,7 @@ function foo() {
 	if (process.env['NODE_ENV'] === 'production') {
 		console.log(123);
 	}
+	const test = process.env['NODE_ENV'];
 }
             "###
             .to_string(),
