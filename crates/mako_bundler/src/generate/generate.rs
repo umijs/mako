af23591454858804e2870a7c500b85d4e3aa18f7
pshu <pishu.spf@antfin.com>
2023-04-23 18:50:52 +0800

    fix: 🐛 inject helpers

--- crates/mako_bundler/src/generate/generate.rs
+++ crates/mako_bundler/src/generate/generate.rs
@@ -97,7 +97,111 @@ const requireModule = (name) => {
   return module.exports;
 };
         "#
-        .to_string()];
+            .to_string(),
+            r#"
+        define("@swc/helpers/lib/_interop_require_default.js", function(module, exports, require) {
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "default", {
+    enumerable: true,
+    get: function() {
+        return _interopRequireDefault;
+    }
+});
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+        default: obj,
+        ...obj,
+    };
+}
+        });
+"#
+            .to_string(),
+            r#"
+        define("@swc/helpers/lib/_export_star.js", function(module, exports, require) {
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "default", {
+    enumerable: true,
+    get: function() {
+        return _exportStar;
+    }
+});
+function _exportStar(from, to) {
+    Object.keys(from).forEach(function(k) {
+        if (k !== "default" && !Object.prototype.hasOwnProperty.call(to, k)) Object.defineProperty(to, k, {
+            enumerable: true,
+            get: function get() {
+                return from[k];
+            }
+        });
+    });
+    return from;
+}
+        });
+"#
+            .to_string(),
+            r#"
+        define("@swc/helpers/lib/_interop_require_wildcard.js", function(module, exports, require) {
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "default", {
+    enumerable: true,
+    get: function() {
+        return _interopRequireWildcard;
+    }
+});
+function _getRequireWildcardCache(nodeInterop) {
+    if (typeof WeakMap !== "function") return null;
+    var cacheBabelInterop = new WeakMap();
+    var cacheNodeInterop = new WeakMap();
+    return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) {
+        return nodeInterop ? cacheNodeInterop : cacheBabelInterop;
+    })(nodeInterop);
+}
+function _interopRequireWildcard(obj, nodeInterop) {
+    if (!nodeInterop && obj && obj.__esModule) {
+        return obj;
+    }
+    if (obj === null || typeof obj !== "object" && typeof obj !== "function") {
+        return {
+            default: obj
+        };
+    }
+    var cache = _getRequireWildcardCache(nodeInterop);
+    if (cache && cache.has(obj)) {
+        return cache.get(obj);
+    }
+    var newObj = {};
+    var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;
+    for(var key in obj){
+        if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) {
+            var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;
+            if (desc && (desc.get || desc.set)) {
+                Object.defineProperty(newObj, key, desc);
+            } else {
+                newObj[key] = obj[key];
+            }
+        }
+    }
+    newObj.default = obj;
+    if (cache) {
+        cache.set(obj, newObj);
+    }
+    return newObj;
+}
+        });
+"#
+                .to_string(),
+
+
+        ];
         let module_ids = self
             .context
             .module_graph
