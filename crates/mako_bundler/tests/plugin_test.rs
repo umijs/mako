3c39b3508bfed71f7aba703a5e842b8d1ef8007a
PeachScript <scdzwyxst@gmail.com>
2023-04-24 22:46:02 +0800

    test: update cases for plugin driver

--- crates/mako_bundler/tests/plugin_test.rs
+++ crates/mako_bundler/tests/plugin_test.rs
@@ -43,12 +43,13 @@ fn test_first_and_before() {
     pd2.register(TestPlugin2 {});
 
     // assert 2 plugin drivers get same order
-    if let Some(ret) = pd1.run_hook_first(|p| p.example_method("p1".to_string())) {
-        assert_eq!(ret, "p1:plugin-2")
-    }
-    if let Some(ret) = pd2.run_hook_first(|p| p.example_method("p2".to_string())) {
-        assert_eq!(ret, "p2:plugin-2")
-    }
+    let ret1 = pd1.run_hook_first(|p| p.example_method("p1".to_string()));
+    assert!(ret1.is_some());
+    assert_eq!(ret1.unwrap(), "p1:plugin-2");
+
+    let ret2 = pd2.run_hook_first(|p| p.example_method("p2".to_string()));
+    assert!(ret2.is_some());
+    assert_eq!(ret2.unwrap(), "p2:plugin-2");
 }
 
 #[test]
@@ -58,34 +59,39 @@ fn test_serial() {
     pd.register(TestPlugin1 {});
     pd.register(TestPlugin2 {});
 
-    let mut ret = vec![];
-    pd.run_hook_serial(|p| {
-        p.example_method("p".to_string()).map(|r| {
-            ret.push(r);
-            Some(())
-        });
+    let ret = pd.run_hook_serial(|p, last_ret| {
+        if p.name() == "mako-test:plugin-2" {
+            // sleep 1ms for plugin-2 to assert plugin-2 still run before plugin-1
+            sleep(Duration::from_millis(1));
+        }
+        p.example_method(last_ret.unwrap_or_else(|| "serial".to_string()))
     });
 
-    assert_eq!(ret, vec!["p:plugin-2", "p:plugin-1"]);
+    // expect hook return value is plugin method return value concat
+    // and the plugin-1 is run after plugin-2
+    assert!(ret.is_some());
+    assert_eq!(ret.unwrap(), "serial:plugin-2:plugin-1");
 }
 
 #[test]
 fn test_parallel() {
+    static mut LAST_PLUGIN: String = String::new();
     let mut pd = PluginDriver::new();
 
     pd.register(TestPlugin1 {});
     pd.register(TestPlugin2 {});
 
-    let mut ret = vec![];
-    pd.run_hook_serial(|p| {
-        if p.name() == "mako-test:plugin-1" {
-            // sleep 1ms to make sure plugin-1 run after plugin-2
+    pd.run_hook_parallel(|p| {
+        // sleep 1ms for plugin-2 to assert plugin-2 run after plugin-1
+        if p.name() == "mako-test:plugin-2" {
             sleep(Duration::from_millis(1));
         }
-        if let Some(r) = p.example_method("p".to_string()) {
-            ret.push(r);
+        p.example_method("p".to_string());
+        unsafe {
+            LAST_PLUGIN = p.name().to_string();
         }
     });
 
-    assert_eq!(ret, vec!["p:plugin-2", "p:plugin-1"]);
+    // expect plugin-2 is finished after plugin-1 even if it configured `before` plugin-1
+    assert_eq!("mako-test:plugin-2", unsafe { LAST_PLUGIN.clone() });
 }
