c761c25b6e36121aaf651a01a410cc3754215bf2
pshu <pishu.spf@antfin.com>
2023-04-24 23:01:40 +0800

    feat: ✨ use lodash and axios

--- examples/with-antd/index.tsx
+++ examples/with-antd/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import Button from 'antd/es/button';
+import lodash from 'lodash';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+
   return (
     <div>
-      <Button> Antd </Button>
+      <Button
+        onClick={() => {
+          axios.get('https://jsonplaceholder.typicode.com/posts/1').then((res) => {
+            setData(res.data);
+          });
+        }}
+      >
+        {lodash.toUpper('antd')}{' '}
+      </Button>
+      <h3>Data from axios</h3>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
