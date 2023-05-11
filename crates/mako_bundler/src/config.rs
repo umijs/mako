ddf3ea5a89074b32076e7e7448520c77964debf5
杰司 <liguoqing.lgq@antgroup.com>
2023-05-11 20:02:51 +0800

    feat: 兼容 mako.config.json 文件不存在场景

--- crates/mako_bundler/src/config.rs
+++ crates/mako_bundler/src/config.rs
@@ -35,7 +35,9 @@ pub struct Config {
 impl Config {
     pub fn from_literal_str(s: &str) -> Result<Self, config::ConfigError> {
         let args: Vec<String> = std::env::args().collect();
-        let root = std::env::current_dir().unwrap().join(&args[1]);
+        let root = std::env::current_dir()
+            .unwrap()
+            .join(args.get(1).unwrap_or(&String::from("mako_project")));
 
         let conf = config::Config::builder()
             .add_source(config::File::from_str(
@@ -43,7 +45,7 @@ impl Config {
                 config::FileFormat::Json,
             ))
             .add_source(config::File::from_str(s, config::FileFormat::Json))
-            .add_source(config::File::from(root.join("mako.config.json")))
+            .add_source(config::File::from(root.join("mako.config.json")).required(false))
             .build()?;
         conf.try_deserialize()
     }
