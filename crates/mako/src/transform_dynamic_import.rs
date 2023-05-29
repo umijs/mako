d17080baf49f5d242e4852a5ebf05d0ea0738246
sorrycc <sorrycc@gmail.com>
2023-05-29 16:23:48 +0800

    test: fix ci

--- crates/mako/src/transform_dynamic_import.rs
+++ crates/mako/src/transform_dynamic_import.rs
@@ -163,8 +163,7 @@ require.ensure([
             ast.visit_mut_with(&mut dyanmic_import);
         });
 
-        let (code, _sourcemap) =
-            js_ast_to_code(&ast, &context.meta.script.cm, &context, "index.js");
+        let (code, _sourcemap) = js_ast_to_code(&ast, &context, "index.js");
         let code = code.replace("\"use strict\";", "");
         let code = code.trim().to_string();
         (code, _sourcemap)
