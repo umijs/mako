8f7934d9499604677380ffaa07d40a834f759ea7
chencheng (云谦) <sorrycc@gmail.com>
2023-06-15 16:22:39 +0800

    fix: improve node polyfill (#142)
    * fix: improve node polyfill
    
    * chore: code style

--- crates/mako/src/config_node_polyfill.rs
+++ crates/mako/src/config_node_polyfill.rs
@@ -3,58 +3,27 @@ use crate::config::Config;
 fn get_node_builtins() -> Vec<String> {
     vec![
         "assert",
-        "assert/strict",
-        "async_hooks",
         "buffer",
-        "child_process",
-        "cluster",
         "console",
         "constants",
         "crypto",
-        "dgram",
-        "diagnostics_channel",
-        "dns",
-        "dns/promises",
         "domain",
         "events",
-        "fs",
-        "fs/promises",
         "http",
-        "http2",
         "https",
-        "inspector",
-        "inspector/promises",
-        "module",
-        "net",
         "os",
         "path",
-        "path/posix",
-        "path/win32",
-        "perf_hooks",
         "process",
         "punycode",
         "querystring",
-        "readline",
-        "readline/promises",
-        "repl",
         "stream",
-        "stream/consumers",
-        "stream/promises",
-        "stream/web",
         "string_decoder",
         "sys",
         "timers",
-        "timers/promises",
-        "tls",
-        "trace_events",
         "tty",
         "url",
         "util",
-        "util/types",
-        "v8",
         "vm",
-        "wasi",
-        "worker_threads",
         "zlib",
     ]
     .iter()
@@ -68,10 +37,14 @@ impl Config {
 
         for name in builtins.iter() {
             config
-                .externals
+                .resolve
+                .alias
                 // why replace / ?
                 // since a/b is not a valid js variable name
-                .insert(name.to_string(), name.to_string().replace('/', "__"));
+                .insert(
+                    name.to_string(),
+                    format!("node-libs-browser-okam/polyfill/{}", name),
+                );
         }
     }
 }
