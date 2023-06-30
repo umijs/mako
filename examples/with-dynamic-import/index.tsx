cdd77b1a9e94548e6fb157f0138ed2a34f21f137
禾登 <bangjun.wbj@antgroup.com>
2023-06-30 16:28:40 +0800

    chore: 删除 dynamic-import example 中的多余 socket client

--- examples/with-dynamic-import/index.tsx
+++ examples/with-dynamic-import/index.tsx
@@ -1,11 +1,1 @@
 import './render';
-const onUpdate = () => {
-  console.time('hmr');
-  module.hot.check().then(() => {
-    console.timeEnd('hmr');
-  });
-};
-
-const socket = new WebSocket('ws://127.0.0.1:3000/__/hmr-ws');
-
-socket.addEventListener('message', onUpdate);
