73fd2a21234383cf8343cd01183ac7af7760491f
Maple0817 <maple.lih@gmail.com>
2023-07-06 16:39:03 +0800

    fix: lightningcss支持nesting

--- crates/mako/src/lightningcss.rs
+++ crates/mako/src/lightningcss.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
 
-use lightningcss::stylesheet::{MinifyOptions, ParserOptions, PrinterOptions, StyleSheet};
+use lightningcss::stylesheet::{
+    MinifyOptions, ParserFlags, ParserOptions, PrinterOptions, StyleSheet,
+};
 
 use crate::compiler::Context;
 use crate::config::Mode;
@@ -9,7 +11,16 @@ use crate::targets;
 pub fn lightingcss_transform(code: &str, context: &Arc<Context>) -> String {
     // something more, lightning will transform @import url() to @import ""
     let targets = targets::lightningcss_targets_from_map(context.config.targets.clone());
-    let mut lightingcss_stylesheet = StyleSheet::parse(code, ParserOptions::default()).unwrap();
+    let mut flags = ParserFlags::empty();
+    flags.set(ParserFlags::NESTING, true);
+    let mut lightingcss_stylesheet = StyleSheet::parse(
+        code,
+        ParserOptions {
+            flags,
+            ..Default::default()
+        },
+    )
+    .unwrap();
     lightingcss_stylesheet
         .minify(MinifyOptions {
             targets,
