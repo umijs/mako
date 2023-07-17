5a713a44cb5b936223b3f9eca71f20a928f0a1aa
chencheng (云谦) <sorrycc@gmail.com>
2023-07-17 14:24:19 +0800

    test: add testcase for load (#261)

---
+++ crates/mako/test/compile/load/index.ts
@@ -0,0 +1,16 @@
+import json from './foo.json';
+console.log(json);
+
+import xml from './foo.xml';
+console.log(xml);
+
+import json5 from './foo.json5';
+console.log(json5);
+
+import yaml from './foo.yaml';
+console.log(yaml);
+
+import './foo.css';
+
+import toml from './foo.toml';
+console.log(toml);
