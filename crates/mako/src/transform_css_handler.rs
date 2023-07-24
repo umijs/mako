2eda9d0f7027f3b3d819fed3c4c52a48717841cd
sorrycc <sorrycc@gmail.com>
2023-07-24 17:37:23 +0800

    fix(css): handle deps and support ~ prefix

--- crates/mako/src/transform_css_handler.rs
+++ crates/mako/src/transform_css_handler.rs
@@ -5,12 +5,12 @@ use swc_common::util::take::Take;
 use swc_css_ast::{AtRulePrelude, ImportHref, Rule, Stylesheet, Url, UrlValue};
 use swc_css_visit::{VisitMut, VisitMutWith};
 
-use crate::analyze_deps::is_url_ignored;
+use crate::analyze_deps::{handle_css_url, is_url_ignored};
 use crate::compiler::Context;
 use crate::load::handle_asset;
 
 pub struct CssHandler<'a> {
-    pub dep_map: HashMap<String, String>,
+    pub assets_map: HashMap<String, String>,
     pub context: &'a Arc<Context>,
 }
 
@@ -55,24 +55,35 @@ impl VisitMut for CssHandler<'_> {
         match n.value {
             Some(box UrlValue::Str(ref mut s)) => {
                 let url = &s.value.to_string();
-                if !is_url_ignored(url) {
-                    if let Some(replacement) = self.dep_map.get(url) {
-                        // CSS url() 里的资源是 css visit 时 handle 的
-                        let asset_content = handle_asset(self.context, replacement);
-                        s.value = asset_content.unwrap_or_else(|_| replacement.clone()).into();
-                        s.raw = None;
-                    }
+                if is_url_ignored(url) {
+                    return;
+                }
+                let url = handle_css_url(url.to_string());
+                println!(
+                    "self.dep_map: {:?}, url: {}, exists: {}",
+                    self.assets_map,
+                    &url,
+                    self.assets_map.get(&url).is_some()
+                );
+                if let Some(replacement) = self.assets_map.get(&url) {
+                    // CSS url() 里的资源是 css visit 时 handle 的
+                    let asset_content = handle_asset(self.context, replacement);
+                    s.value = asset_content.unwrap_or_else(|_| replacement.clone()).into();
+                    println!("s.value: {}", &s.value);
+                    s.raw = None;
                 }
             }
             Some(box UrlValue::Raw(ref mut s)) => {
                 let url = &s.value.to_string();
-                if !is_url_ignored(url) {
-                    if let Some(replacement) = self.dep_map.get(url) {
-                        // CSS url() 里的资源是 css visit 时 handle 的
-                        let asset_content = handle_asset(self.context, replacement);
-                        s.value = asset_content.unwrap_or_else(|_| replacement.clone()).into();
-                        s.raw = None;
-                    }
+                if is_url_ignored(url) {
+                    return;
+                }
+                let url = handle_css_url(url.to_string());
+                if let Some(replacement) = self.assets_map.get(&url) {
+                    // CSS url() 里的资源是 css visit 时 handle 的
+                    let asset_content = handle_asset(self.context, replacement);
+                    s.value = asset_content.unwrap_or_else(|_| replacement.clone()).into();
+                    s.raw = None;
                 }
             }
             None => {}
