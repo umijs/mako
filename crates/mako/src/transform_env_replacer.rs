9b9045e28f499cb499e2bf7a6d35cf9cd8ae36cc
pshu <stormslowly@gmail.com>
2023-08-04 12:17:30 +0800

    fix(define): treat define value as code (#321)
    * feat: ✨ create config Error
    
    * feat: ✨ treat define string as code
    
    ---------
    
    Co-authored-by: pshu <pishu.spf@antfin.com>

--- crates/mako/src/transform_env_replacer.rs
+++ crates/mako/src/transform_env_replacer.rs
@@ -1,13 +1,23 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+
+use anyhow::{anyhow, Result};
+use serde_json::Value;
 use swc_atoms::{js_word, JsWord};
 use swc_common::collections::{AHashMap, AHashSet};
 use swc_common::sync::Lrc;
 use swc_common::DUMMY_SP;
 use swc_ecma_ast::{
-    ComputedPropName, Expr, Id, Ident, KeyValueProp, Lit, MemberExpr, MemberProp, MetaPropExpr,
-    MetaPropKind, Module, ObjectLit, Prop, PropName, PropOrSpread, Str,
+    ArrayLit, Bool, ComputedPropName, Expr, ExprOrSpread, Id, Ident, KeyValueProp, Lit, MemberExpr,
+    MemberProp, MetaPropExpr, MetaPropKind, Module, ModuleItem, Null, Number, ObjectLit, Prop,
+    PropName, PropOrSpread, Stmt, Str,
 };
-use swc_ecma_utils::collect_decls;
+use swc_ecma_utils::{collect_decls, ExprExt};
 use swc_ecma_visit::{VisitMut, VisitMutWith};
+
+use crate::ast::build_js_ast;
+use crate::compiler::Context;
+use crate::config::ConfigError;
 
 enum EnvsType {
     Node(Lrc<AHashMap<JsWord, Expr>>),
@@ -170,5 +180,110 @@ impl VisitMut for EnvReplacer {
         }
 
         expr.visit_mut_children_with(self);
+    }
+}
+
+pub fn build_env_map(
+    env_map: HashMap<String, Value>,
+    context: &Arc<Context>,
+) -> Result<AHashMap<JsWord, Expr>> {
+    let mut map = AHashMap::default();
+    for (k, v) in env_map.into_iter() {
+        let expr = get_env_expr(v, context)?;
+        map.insert(k.into(), expr);
+    }
+    Ok(map)
+}
+
+fn get_env_expr(v: Value, context: &Arc<Context>) -> Result<Expr> {
+    match v {
+        Value::String(v) => {
+            let ast = build_js_ast("_define_.js", &v, context).unwrap();
+            let module = ast.ast.body.get(0).unwrap();
+
+            match module {
+                ModuleItem::Stmt(Stmt::Expr(stmt_expr)) => {
+                    return Ok(stmt_expr.expr.as_expr().clone());
+                }
+                _ => Err(anyhow!(ConfigError::InvalidateDefineConfig(v))),
+            }
+        }
+        Value::Bool(v) => Ok(Expr::Lit(Lit::Bool(Bool {
+            span: DUMMY_SP,
+            value: v,
+        }))),
+        Value::Number(v) => Ok(Expr::Lit(Lit::Num(Number {
+            span: DUMMY_SP,
+            raw: None,
+            value: v.as_f64().unwrap(),
+        }))),
+        Value::Array(val) => {
+            let mut elems = vec![];
+            for item in val.iter() {
+                elems.push(Some(ExprOrSpread {
+                    spread: None,
+                    expr: Box::new(get_env_expr(item.clone(), context)?),
+                }));
+            }
+            Ok(Expr::Array(ArrayLit {
+                span: DUMMY_SP,
+                elems,
+            }))
+        }
+        Value::Null => Ok(Expr::Lit(Lit::Null(Null { span: DUMMY_SP }))),
+        Value::Object(val) => {
+            let mut props = vec![];
+            for (key, value) in val.iter() {
+                let prop = PropOrSpread::Prop(Box::new(Prop::KeyValue(KeyValueProp {
+                    key: PropName::Ident(Ident::new(key.clone().into(), DUMMY_SP)),
+                    value: Box::new(get_env_expr(value.clone(), context)?),
+                })));
+                props.push(prop);
+            }
+            Ok(Expr::Object(ObjectLit {
+                span: DUMMY_SP,
+                props,
+            }))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use maplit::hashmap;
+    use serde_json::json;
+
+    use crate::compiler::Context;
+    use crate::transform_env_replacer::build_env_map;
+
+    #[should_panic = "define value 'for(;;)console.log()' is not a Expression"]
+    #[test]
+    fn test_wrong_define_value() {
+        let context: Arc<Context> = Arc::new(Default::default());
+
+        build_env_map(
+            hashmap! {
+                "wrong".to_string() => json!("for(;;)console.log()")
+            },
+            &context,
+        )
+        .unwrap();
+    }
+
+    #[should_panic = "define value 'for(;;)console.log()' is not a Expression"]
+    #[test]
+    fn test_nested_wrong_define_value() {
+        let context: Arc<Context> = Arc::new(Default::default());
+
+        build_env_map(
+            hashmap! {
+                "parent".to_string() =>
+                json!({"wrong": "for(;;)console.log()" })
+            },
+            &context,
+        )
+        .unwrap();
     }
 }
