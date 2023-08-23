bedc75abf098735347f6b649c2e699717a4a9d3d
yuyuehui.yyh <yuyuehui.yyh@digital-engine.com>
2023-08-23 16:16:26 +0800

    feat: 去除 dev 时不生成 hash 值的判定

--- crates/mako/src/generate.rs
+++ crates/mako/src/generate.rs
@@ -102,7 +102,7 @@ impl Compiler {
         let t_ast_to_code_and_write = Instant::now();
         debug!("ast to code and write");
         chunk_asts.par_iter().try_for_each(|file| -> Result<()> {
-            for file in get_chunk_emit_files(file, &self.context, true)? {
+            for file in get_chunk_emit_files(file, &self.context)? {
                 self.write_to_dist_with_stats(file);
             }
 
@@ -189,7 +189,7 @@ impl Compiler {
         let t_ast_to_code_and_write = Instant::now();
         debug!("ast to code and write");
         chunk_asts.par_iter().try_for_each(|file| -> Result<()> {
-            for file in get_chunk_emit_files(file, &self.context, false)? {
+            for file in get_chunk_emit_files(file, &self.context)? {
                 self.write_to_dist_with_stats(file);
             }
 
@@ -395,19 +395,14 @@ fn to_hot_update_chunk_name(chunk_name: &String, hash: u64) -> String {
 )]
 // build时，需要产生 hash 值, dev 时不需要 hash 置为 false
 // 需要在这里提前记录 js 和 map 的 hash，因为 map 是不单独计算 hash 值的，继承的是 js 的 hash 值
-fn get_chunk_emit_files(
-    file: &OutputAst,
-    context: &Arc<Context>,
-    hash: bool,
-) -> Result<Vec<EmitFile>> {
+fn get_chunk_emit_files(file: &OutputAst, context: &Arc<Context>) -> Result<Vec<EmitFile>> {
     let mut files = vec![];
-    let need_hash = context.config.hash && hash;
     match &file.ast {
         ModuleAst::Script(ast) => {
             // ast to code
             let (js_code, js_sourcemap) = js_ast_to_code(&ast.ast, context, &file.path)?;
             // 计算 hash 值
-            let hashname = match need_hash {
+            let hashname = match context.config.hash {
                 true => hash_file_name(file.path.clone(), get_content_hash(js_code.clone())),
                 _ => file.path.clone(),
             };
@@ -431,7 +426,7 @@ fn get_chunk_emit_files(
             // ast to code
             let (css_code, css_sourcemap) = css_ast_to_code(ast, context, &file.path);
             // 计算 hash 值
-            let hashname = match need_hash {
+            let hashname = match context.config.hash {
                 true => hash_file_name(file.path.clone(), get_content_hash(css_code.clone())),
                 _ => file.path.clone(),
             };
