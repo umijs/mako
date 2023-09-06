e43401e5849a09048f7b487a4e783c74593e996e
PeachScript <scdzwyxst@gmail.com>
2023-09-06 11:13:39 +0800

    feat: optimize modules to chunk info

--- crates/mako/src/optimize_chunk.rs
+++ crates/mako/src/optimize_chunk.rs
@@ -1,8 +1,14 @@
+use std::collections::HashMap;
+use std::string::String;
+
+use indexmap::{IndexMap, IndexSet};
+use nodejs_resolver::Resource;
 use regex::Regex;
 
 use crate::chunk::{Chunk, ChunkId, ChunkType};
 use crate::compiler::Compiler;
-use crate::module::ModuleId;
+use crate::module::{Module, ModuleId, ModuleInfo};
+use crate::resolve::{ResolvedResource, ResolverResource};
 
 #[derive(Clone)]
 pub enum OptimizeAllowChunks {
@@ -32,6 +38,7 @@ pub struct OptimizeChunksInfo {
     pub chunk_modules: Vec<OptimizeChunkModule>,
 }
 
+#[derive(PartialEq, Eq, Clone)]
 pub struct OptimizeChunkModule {
     pub module_id: ModuleId,
     pub chunk_ids: Vec<ChunkId>,
@@ -54,6 +61,12 @@ impl Compiler {
 
             // stage: deasync
             self.merge_minimal_async_chunks(&optimize_options);
+
+            // stage: modules
+            self.module_to_optimize_infos(&mut optimize_chunks_infos);
+
+            // stage: size
+            self.optimize_chunk_size(&mut optimize_chunks_infos);
         }
     }
 
@@ -97,7 +110,207 @@ impl Compiler {
         }
     }
 
+    fn module_to_optimize_infos<'a>(
+        &'a self,
+        optimize_chunks_infos: &'a mut Vec<OptimizeChunksInfo>,
+    ) {
+        let chunk_graph = self.context.chunk_graph.read().unwrap();
+        let chunks = chunk_graph.get_chunks();
+        let modules_in_chunk = chunks.iter().fold(vec![], |mut acc, chunk| {
+            acc.append(
+                &mut chunk
+                    .modules
+                    .iter()
+                    .map(|m| (m, &chunk.id, &chunk.chunk_type))
+                    .collect::<Vec<_>>(),
+            );
+            acc
+        });
+
+        for (module_id, chunk_id, chunk_type) in modules_in_chunk {
+            for optimize_info in &mut *optimize_chunks_infos {
+                // check test regex
+                if let Some(test) = &optimize_info.group_options.test {
+                    if !test.is_match(&module_id.id.to_string()) {
+                        continue;
+                    }
+                }
+
+                // check min shared count of chunks
+                if optimize_info.group_options.min_chunks > 0
+                    && chunks
+                        .iter()
+                        .filter(|chunk| {
+                            self.check_chunk_type_allow(
+                                &optimize_info.group_options.allow_chunks,
+                                &chunk.chunk_type,
+                            ) && chunk.has_module(module_id)
+                        })
+                        .count()
+                        < optimize_info.group_options.min_chunks
+                {
+                    continue;
+                }
+
+                // check allow chunks
+                if self
+                    .check_chunk_type_allow(&optimize_info.group_options.allow_chunks, chunk_type)
+                {
+                    // save module to optimize info
+                    if let Some(chunk_module) = optimize_info
+                        .chunk_modules
+                        .iter_mut()
+                        .find(|cm| cm.module_id.id == module_id.id)
+                    {
+                        chunk_module.chunk_ids.push(chunk_id.clone());
+                    } else {
+                        optimize_info.chunk_modules.push(OptimizeChunkModule {
+                            module_id: module_id.clone(),
+                            chunk_ids: vec![chunk_id.clone()],
+                        });
+                    }
+                    // only add to one group
+                    break;
+                }
+            }
+        }
+    }
+
+    fn optimize_chunk_size<'a>(&'a self, optimize_chunks_infos: &'a mut Vec<OptimizeChunksInfo>) {
+        let chunk_size_map = optimize_chunks_infos
+            .iter()
+            .map(|info| {
+                let info_chunk = &Chunk {
+                    modules: info
+                        .chunk_modules
+                        .iter()
+                        .map(|cm| cm.module_id.clone())
+                        .collect::<IndexSet<_>>(),
+                    id: ChunkId { id: "".to_string() },
+                    chunk_type: ChunkType::Sync,
+                    content: None,
+                    source_map: None,
+                };
+
+                (
+                    info.group_options.name.clone(),
+                    self.get_chunk_size(info_chunk),
+                )
+            })
+            .collect::<HashMap<_, _>>();
+
+        // drop optimize infos if chunk size is less than min_size
+        optimize_chunks_infos.retain(|info| {
+            *chunk_size_map.get(&info.group_options.name).unwrap() >= info.group_options.min_size
+        });
+
+        // continue split chunk if chunk size is greater than max_size
+        let mut extra_optimize_infos = vec![];
+        let module_graph = self.context.module_graph.read().unwrap();
+        for mut info in &mut *optimize_chunks_infos {
+            let mut split_chunk_count = 0;
+            let mut chunk_size = *chunk_size_map.get(&info.group_options.name).unwrap();
+
+            if chunk_size > info.group_options.max_size {
+                let chunk_modules = &info.chunk_modules;
+                // group size by package name
+                let mut package_size_map = chunk_modules.iter().fold(
+                    IndexMap::<String, (usize, Vec<OptimizeChunkModule>)>::new(),
+                    |mut size_map, cm| {
+                        let pkg_name = match module_graph.get_module(&cm.module_id) {
+                            Some(Module {
+                                info:
+                                    Some(ModuleInfo {
+                                        resolved_resource:
+                                            Some(ResolverResource::Resolved(ResolvedResource(
+                                                Resource {
+                                                    description: Some(module_desc),
+                                                    ..
+                                                },
+                                            ))),
+                                        ..
+                                    }),
+                                ..
+                            }) => module_desc.data().raw().get("name"),
+                            _ => None,
+                        }
+                        .map(|n| n.as_str().unwrap())
+                        .unwrap_or("unknown");
+
+                        let module_size = module_graph
+                            .get_module(&cm.module_id)
+                            .unwrap()
+                            .get_module_size();
+
+                        // add module size to package size
+                        if let Some((item, modules)) = size_map.get_mut(pkg_name) {
+                            *item += module_size;
+                            modules.push(cm.clone());
+                        } else {
+                            size_map.insert(pkg_name.to_string(), (module_size, vec![cm.clone()]));
+                        }
+                        size_map
+                    },
+                );
+
+                // split new chunks until chunk size is less than max_size and there has more than 1 package can be split
+                while chunk_size > info.group_options.max_size && package_size_map.len() > 1 {
+                    let mut new_chunk_size = 0;
+                    let mut new_chunk_modules = vec![];
+
+                    // collect modules by package name until chunk size is very to max_size
+                    while !package_size_map.is_empty()
+                        && new_chunk_size + package_size_map.get_index(0).unwrap().1 .0
+                            < info.group_options.max_size
+                    {
+                        let (_, (size, modules)) = package_size_map.swap_remove_index(0).unwrap();
+
+                        new_chunk_size += size;
+                        new_chunk_modules.append(&mut modules.clone());
+                    }
+
+                    // clone group options for new chunk
+                    let mut new_chunk_group_options = info.group_options.clone();
+                    new_chunk_group_options.name =
+                        format!("{}_{}", info.group_options.name, split_chunk_count);
+
+                    // update original chunk size and split chunk count
+                    chunk_size -= new_chunk_size;
+                    split_chunk_count += 1;
+
+                    // move modules to new chunk
+                    info.chunk_modules
+                        .retain(|cm| !new_chunk_modules.contains(cm));
+                    extra_optimize_infos.push(OptimizeChunksInfo {
+                        group_options: new_chunk_group_options,
+                        chunk_modules: new_chunk_modules,
+                    });
+                }
+
+                // rename original chunk if it has been split
+                if split_chunk_count > 0 {
+                    info.group_options.name =
+                        format!("{}_{}", info.group_options.name, split_chunk_count);
+                }
+            }
+        }
+
+        // add extra optimize infos
+        optimize_chunks_infos.extend(extra_optimize_infos);
+    }
+
     /* the following is util methods */
+
+    fn check_chunk_type_allow(
+        &self,
+        allow_chunks: &OptimizeAllowChunks,
+        chunk_type: &ChunkType,
+    ) -> bool {
+        match allow_chunks {
+            OptimizeAllowChunks::Entry => chunk_type == &ChunkType::Entry,
+            OptimizeAllowChunks::Async => chunk_type == &ChunkType::Async,
+        }
+    }
 
     fn get_chunk_size(&self, chunk: &Chunk) -> usize {
         let module_graph = self.context.module_graph.read().unwrap();
