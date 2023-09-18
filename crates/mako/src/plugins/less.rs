9deeba53aff6f9f005eaed11c40f25a7f51913a3
chencheng (云谦) <sorrycc@gmail.com>
2023-09-18 18:19:36 +0800

    perf: cache less compile (#468)

--- crates/mako/src/plugins/less.rs
+++ crates/mako/src/plugins/less.rs
@@ -2,9 +2,10 @@ use std::process::Command;
 use std::sync::Arc;
 
 use anyhow::{anyhow, Result};
+use cached::proc_macro::cached;
 
 use crate::compiler::Context;
-use crate::load::{Content, LoadError};
+use crate::load::{read_content, Content, LoadError};
 use crate::plugin::{Plugin, PluginLoadParam};
 
 pub struct LessPlugin {}
@@ -16,44 +17,55 @@ impl Plugin for LessPlugin {
 
     fn load(&self, param: &PluginLoadParam, context: &Arc<Context>) -> Result<Option<Content>> {
         if matches!(param.ext_name.as_str(), "less") {
-            // compile less to css
-            let mut cmd = Command::new("npx");
-            cmd.current_dir(context.root.clone());
-            let theme = context.config.less.theme.clone();
-            let vars = theme
-                .iter()
-                .map(|(k, v)| format!("{}={}", k, v))
-                .collect::<Vec<String>>()
-                .join("&");
-            cmd.args([
-                "lessc",
-                "--js",
-                format!("--modify-var={}", vars).as_str(),
-                &param.path,
-            ]);
-
-            let output = match cmd.output() {
-                Ok(output) => output,
-                Err(reason) => {
-                    return Err(anyhow!(LoadError::CompileLessError {
-                        path: param.path.to_string(),
-                        reason: reason.to_string(),
-                    }));
-                }
-            };
-            if !output.status.success() {
-                let mut reason = String::from_utf8_lossy(&output.stderr).to_string();
-                if reason.contains("could not determine executable to run") {
-                    reason = "lessc is not found, please install less dependency".to_string();
-                }
-                return Err(anyhow!(LoadError::CompileLessError {
-                    path: param.path.to_string(),
-                    reason,
-                }));
-            }
-            let css_content = String::from_utf8_lossy(&output.stdout);
-            return Ok(Some(Content::Css(css_content.into())));
+            let content = read_content(param.path.as_str())?;
+            let css_content = compile_less(param, content.as_str(), context)?;
+            return Ok(Some(Content::Css(css_content)));
         }
         Ok(None)
     }
+}
+
+#[cached(
+    result = true,
+    key = "String",
+    convert = r#"{ format!("{}-{}", param.path, _content) }"#
+)]
+fn compile_less(param: &PluginLoadParam, _content: &str, context: &Arc<Context>) -> Result<String> {
+    let mut cmd = Command::new("npx");
+    cmd.current_dir(context.root.clone());
+    let theme = context.config.less.theme.clone();
+    let mut args = Vec::from(["lessc".to_string(), "--js".to_string()]);
+    if !theme.is_empty() {
+        let vars = theme
+            .iter()
+            .map(|(k, v)| format!("{}={}", k, v))
+            .collect::<Vec<String>>()
+            .join("&");
+        let modify_vars = format!("--modify-var={}", vars);
+        args.push(modify_vars);
+    }
+    args.push(param.path.to_string());
+    cmd.args(args);
+
+    let output = match cmd.output() {
+        Ok(output) => output,
+        Err(reason) => {
+            return Err(anyhow!(LoadError::CompileLessError {
+                path: param.path.to_string(),
+                reason: reason.to_string(),
+            }));
+        }
+    };
+    if !output.status.success() {
+        let mut reason = String::from_utf8_lossy(&output.stderr).to_string();
+        if reason.contains("could not determine executable to run") {
+            reason = "lessc is not found, please install less dependency".to_string();
+        }
+        return Err(anyhow!(LoadError::CompileLessError {
+            path: param.path.to_string(),
+            reason,
+        }));
+    }
+    let css_content = String::from_utf8_lossy(&output.stdout);
+    Ok(css_content.into())
 }
