51ecbce82adca822164c0869dbd9b1140319072e
杰司 <liguoqing.lgq@antgroup.com>
2023-09-21 15:50:10 +0800

    fix: fix infinite loop

--- crates/mako/src/optimize_chunk.rs
+++ crates/mako/src/optimize_chunk.rs
@@ -276,8 +276,9 @@ impl Compiler {
 
                     // collect modules by package name until chunk size is very to max_size
                     while !package_size_map.is_empty()
-                        && new_chunk_size + package_size_map.get_index(0).unwrap().1 .0
-                            < info.group_options.max_size
+                        && (new_chunk_size == 0
+                            || new_chunk_size + package_size_map.get_index(0).unwrap().1 .0
+                                < info.group_options.max_size)
                     {
                         let (_, (size, modules)) = package_size_map.swap_remove_index(0).unwrap();
 
