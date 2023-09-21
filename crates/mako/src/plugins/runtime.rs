05acb5d82dfd3722fcfce5de529702d8d1a44f3d
pshu <stormslowly@gmail.com>
2023-09-21 17:02:19 +0800

    fix: 🐛 public path runtime fallback (#486)
    * fix: 🐛 public path runtime fallback
    
    * Update crates/mako/src/plugins/runtime.rs
    
    * ci: merge master and fmt
    
    ---------
    
    Co-authored-by: pshu <pishu.spf@antfin.com>
    Co-authored-by: chencheng (云谦) <sorrycc@gmail.com>
    Co-authored-by: 杰司 <liguoqing.lgq@antgroup.com>

--- crates/mako/src/plugins/runtime.rs
+++ crates/mako/src/plugins/runtime.rs
@@ -41,7 +41,7 @@ impl MakoRuntime {
     fn public_path(&self, context: &Arc<Context>) -> String {
         let public_path = context.config.public_path.clone();
         let public_path = if public_path == "runtime" {
-            "globalThis.publicPath".to_string()
+            "(typeof globalThis !== 'undefined' ? globalThis : self).publicPath || '/'".to_string()
         } else {
             format!("\"{}\"", public_path)
         };
