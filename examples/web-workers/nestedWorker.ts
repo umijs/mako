155a8ce1bea30a42f6ef1fcb1530afa2ba880f54
杰司 <liguoqing.lgq@antgroup.com>
2023-11-14 14:49:37 +0800

    fix: 不处理 new Worker() 场景

--- examples/web-workers/nestedWorker.ts
+++ examples/web-workers/nestedWorker.ts
@@ -1,6 +1,6 @@
 addEventListener('message', (message) => {
   if ((message.data.command = 'start')) {
-    const worker = new Worker('./commonWorker.ts');
+    const worker = new Worker(new URL('./worker.ts', import.meta.url));
     worker.addEventListener('message', (message) => {
       self.postMessage(message.data);
     });
