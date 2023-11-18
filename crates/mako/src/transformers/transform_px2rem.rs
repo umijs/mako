bb1c8d4758aef4791d290f5ec1aa6fb3d3f9bdfa
zhangpanweb <37805064+zhangpanweb@users.noreply.github.com>
2023-11-18 14:00:29 +0800

    fix: fix px2rem in var (#691)
    * fix: fix px2rem in var
    
    * fix: add whitelist judgement before transforming
    
    * feat: add convertion of value and unit
    
    * fix: should not convert em in var

--- crates/mako/src/transformers/transform_px2rem.rs
+++ crates/mako/src/transformers/transform_px2rem.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 
-use mako_core::swc_css_ast;
-use mako_core::swc_css_ast::Length;
+use mako_core::swc_css_ast::{self, Length, Token};
 use mako_core::swc_css_visit::{VisitMut, VisitMutWith};
 
 use crate::compiler::Context;
@@ -54,5 +53,17 @@ impl VisitMut for Px2Rem<'_> {
             n.unit.value = "rem".into();
         }
         n.visit_mut_children_with(self);
+    }
+    fn visit_mut_token(&mut self, t: &mut Token) {
+        if let Token::Dimension(dimension) = t {
+            if dimension.unit.to_string() == "px" && self.should_transform() {
+                let rem_val = dimension.value / self.context.config.px2rem_config.root;
+                dimension.raw_value = rem_val.to_string().into();
+                dimension.value = rem_val;
+                dimension.raw_unit = "rem".into();
+                dimension.unit = "rem".into();
+            }
+        }
+        t.visit_mut_children_with(self);
     }
 }
