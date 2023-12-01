9dd9ac4af109760fa04f5fe736d3fcdf8cf6d842
pshu <pishu.spf@antfin.com>
2023-12-01 17:00:44 +0800

    release: @okamjs/okam@0.1.8-dev.20231201.1

--- crates/node/index.d.ts
+++ crates/node/index.d.ts
@@ -13,7 +13,6 @@ export interface JsHooks {
       endTime: number;
     };
   }) => void;
-  onGenerateFsWrite?: (path: string, content: Buffer) => Promise<void>;
 }
 export interface BuildParams {
   root: string;
@@ -94,9 +93,19 @@ export interface BuildParams {
       metaPath?: string;
       inject?: Record<
         string,
-        | { from: string; exclude?: string }
-        | { from: string; named: string; exclude?: string }
-        | { from: string; namespace: true; exclude?: string }
+        | { from: string; exclude?: string; preferRequire?: boolean }
+        | {
+            from: string;
+            named: string;
+            exclude?: string;
+            preferRequire?: boolean;
+          }
+        | {
+            from: string;
+            namespace: true;
+            exclude?: string;
+            preferRequire?: boolean;
+          }
       >;
     };
   };
