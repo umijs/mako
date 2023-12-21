c8271ea254575846160ffbd899ec8b48774e4dfb
pshu <pishu.spf@antfin.com>
2023-12-21 10:38:40 +0800

    test: ✅ 更正e2e assert

--- e2e/fixtures/config.dynamic_import_to_require/expect.js
+++ e2e/fixtures/config.dynamic_import_to_require/expect.js
@@ -1,20 +1,18 @@
-const assert = require("assert");
-const { parseBuildResult, moduleReg } = require("../../../scripts/test-utils");
+const assert = require('assert');
+const { parseBuildResult, moduleReg } = require('../../../scripts/test-utils');
 const { files } = parseBuildResult(__dirname);
 
+assert.deepEqual(
+  Object.keys(files).sort(),
+  ['index.js', 'index.js.map'],
+  'no extract chunk generated',
+);
+
 assert(
-  !(`src_foo_js-async.js` in files),
-  "should not have file: src_foo_js-async.js"
+  files['index.js'].includes(`"node_modules/foo/index.js":`),
+  'dynamic imported module(foo) not existss',
 );
 assert(
-  !(`src_foo_js-async.js.map` in files),
-  "should not have file: src_foo_js-async.js.map"
-);
-assert(
-  !(`node_modules_foo_index_js-async.js` in files),
-  "should not have file: node_modules_foo_index_js-async.js"
-);
-assert(
-  !(`node_modules_foo_index_js-async.js.map` in files),
-  "should not have file: node_modules_foo_index_js-async.js.map"
+  files['index.js'].includes(`"src/foo.js":`),
+  'dynamic imported module(./foo) not exists',
 );
