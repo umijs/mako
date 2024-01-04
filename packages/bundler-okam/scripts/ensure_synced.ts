0de97bf01a1cd307d2463a5b66b7807ae5ca45aa
pshu <stormslowly@gmail.com>
2024-01-04 17:51:26 +0800

    chore(scripts): sync okam before pub bundler (#843)
    * chore: 🔧 script check okam whether synced
    
    * refactor: 🎨 slient sync logs
    
    * chore: 🔧 sync optinal deps in scripts

---
+++ packages/bundler-okam/scripts/ensure_synced.ts
@@ -0,0 +1,31 @@
+import 'zx/globals';
+
+(async () => {
+  const makoVersion =
+    require('../package.json')['dependencies']['@okamjs/okam'];
+
+  await retry(3, () => {
+    return (async () => {
+      await $`tnpm sync @okamjs/okam`.quiet();
+      const info =
+        await $`tnpm info --json @okamjs/okam@${makoVersion}`.quiet();
+
+      const optionDeps = JSON.parse(info.stdout)['optionalDependencies'];
+
+      await Promise.all(
+        Object.keys(optionDeps).map((key) => {
+          return $`tnpm info ${key}@${optionDeps[key]}`.quiet();
+        }),
+      );
+    })();
+  });
+
+  console.error(chalk.bgGreen(chalk.white('SUCCEED')), chalk.green('synced'));
+})().catch((err) => {
+  console.error(err);
+  console.error(
+    chalk.bgRed(chalk.white('FAILED')),
+    chalk.red('sync @okamjs/okam failed!'),
+  );
+  process.exit(1);
+});
