5a4920c3f229cfc4e9d6037c89c47ba05a7fc10f
pshu <pishu.spf@antfin.com>
2024-01-04 15:29:55 +0800

    refactor: 🎨 take care of use directive

--- crates/mako/src/transformers/transform_optimize_define_utils.rs
+++ crates/mako/src/transformers/transform_optimize_define_utils.rs
@@ -1,6 +1,6 @@
 use swc_core::common::{Mark, DUMMY_SP};
 use swc_core::ecma::ast::{CallExpr, Expr, ExprOrSpread, Lit, ModuleItem};
-use swc_core::ecma::utils::member_expr;
+use swc_core::ecma::utils::{member_expr, IsDirective};
 use swc_core::ecma::visit::VisitMut;
 
 pub struct OptimizeDefineUtils {
@@ -82,7 +82,20 @@ fn is_obj_lit_arg(arg: Option<&ExprOrSpread>) -> bool {
 
 impl VisitMut for OptimizeDefineUtils {
     fn visit_mut_module_items(&mut self, items: &mut Vec<ModuleItem>) {
-        if let Some(item) = items.get_mut(0)
+        let mut no_directive_index = 0;
+        for (index, item) in items.iter().enumerate() {
+            if let Some(stmt) = item.as_stmt()
+                && stmt.is_directive()
+            {
+                no_directive_index = index + 1
+            } else {
+                break;
+            }
+        }
+
+        let mut iter = items.iter_mut().skip(no_directive_index);
+
+        if let Some(item) = iter.next()
             && let Some(call_expr) = as_call_expr(item)
         {
             // Object.defineProperty(exports, "__esModule", { value: true })
@@ -104,8 +117,10 @@ impl VisitMut for OptimizeDefineUtils {
         }
 
         // cjs will inject most 6 stmts, so we only need to check the next 5 stmts
-        for i in items.iter_mut().skip(1).take(5) {
+        // ref https://github.com/swc-project/swc/blob/ce76159e98ed6cc32104c5da848fe18ebea833b5/crates/swc_ecma_transforms_module/src/common_js.rs#L96
+        for i in iter.take(5) {
             if let Some(call_expr) = as_call_expr(i) {
+                // Object.defineProperty(exports, "default", {...})
                 if let Some(callee_expr) = call_expr.callee.as_expr()
                     && call_expr.args.len() == 3
                     && is_object_define(callee_expr)
@@ -115,16 +130,17 @@ impl VisitMut for OptimizeDefineUtils {
                 {
                     call_expr.callee =
                         member_expr!(DUMMY_SP.apply_mark(self.unresolved_mark), require.d).into();
-
-                    continue;
+                    return;
                 }
 
+                // _export(exports,{...})
                 if let Some(callee_expr) = call_expr.callee.as_expr()
                     && let Some(callee_ident) = callee_expr.as_ident()
                     && call_expr.args.len() == 2
                     && is_export_arg(call_expr.args.first())
                     && is_obj_lit_arg(call_expr.args.get(1))
                     && callee_ident.sym.to_string() == "_export"
+                    // is private ident
                     && !callee_ident
                         .span
                         .ctxt
@@ -132,7 +148,8 @@ impl VisitMut for OptimizeDefineUtils {
                         .is_descendant_of(self.top_level_mark)
                 {
                     call_expr.callee =
-                        member_expr!(DUMMY_SP.apply_mark(self.unresolved_mark), require.e).into()
+                        member_expr!(DUMMY_SP.apply_mark(self.unresolved_mark), require.e).into();
+                    return;
                 }
             }
         }
