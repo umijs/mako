d62badf46a6a3633a0d2f4f656c074266d13be3d
pshu <pishu.spf@antfin.com>
2024-01-09 16:29:24 +0800

    chore: ✏️

--- e2e/fixtures/tree-shaking.skip_module.reexport_same_name_twice/expect.js
+++ e2e/fixtures/tree-shaking.skip_module.reexport_same_name_twice/expect.js
@@ -6,5 +6,5 @@ const content = files['index.js'];
 
 assert(!content.includes('src/dep/proxy.js'), `should skip middle files`);
 
-assert(content.match(/v1:\s+function\(\) {\s+return _dep\.default;\s+}/), `shoule export v1 ref to _dep.default`)
-assert(content.match(/v2:\s+function\(\) {\s+return _dep\.default;\s+}/), `shoule export v2 ref to _dep.default`)
+assert(content.match(/v1:\s+function\(\) {\s+return _dep\.default;\s+}/), `should export v1 ref to _dep.default`)
+assert(content.match(/v2:\s+function\(\) {\s+return _dep\.default;\s+}/), `should export v2 ref to _dep.default`)
