6e90d79c0e5a0133bd116681bfd5c970f50799e6
pshu <pishu.spf@antfin.com>
2024-01-12 15:23:58 +0800

    test: ✅ add lodash-es default import

--- e2e/fixtures/tree-shaking.skip_module.dont_reexport_when_default_import_from_sideeffects_module/expect.js
+++ e2e/fixtures/tree-shaking.skip_module.dont_reexport_when_default_import_from_sideeffects_module/expect.js
@@ -4,7 +4,10 @@ const { files } = parseBuildResult(__dirname);
 
 const content = files['index.js'];
 
-assert(!content.includes('node_modules/pure'), `should skip pure module`);
+assert(
+  content.includes('node_modules/pure/index.js'),
+  `should not skip pure module when it use default import`,
+);
 assert(
   content.includes('node_modules/side_effects/index.js'),
   `should keep all side effects modules`,
