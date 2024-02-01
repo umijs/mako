9aee45830c261acc79178e6924d5a2d8e0133746
pshu <stormslowly@gmail.com>
2024-02-01 16:16:20 +0800

    revert: ⏪ update swc from 83 to 86 (#907)

--- crates/mako/src/plugins/node_stuff.rs
+++ crates/mako/src/plugins/node_stuff.rs
@@ -63,8 +63,8 @@ impl VisitMut for NodeStuff<'_> {
         if let Expr::Ident(ident) = expr
             && ident.span.ctxt.outer() == self.unresolved_mark
         {
-            let is_filename = ident.sym == "__filename";
-            let is_dirname = ident.sym == "__dirname";
+            let is_filename = ident.sym.to_string() == "__filename";
+            let is_dirname = ident.sym.to_string() == "__dirname";
             if is_filename || is_dirname {
                 let path = diff_paths(self.current_path, self.root).unwrap_or("".into());
                 let value = if is_filename {
