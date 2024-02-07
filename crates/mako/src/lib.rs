601efc56e77ba490e45c7e2a4a52ab727db2ba9f
xusd320 <xusd320@gmail.com>
2024-02-07 18:06:46 +0800

    fix: move allocation declare to lib entry

--- crates/mako/src/lib.rs
+++ crates/mako/src/lib.rs
@@ -3,18 +3,6 @@
 #![feature(let_chains)]
 #![feature(result_option_inspect)]
 
-#[cfg(not(target_os = "linux"))]
-#[global_allocator]
-static GLOBAL: mimalloc_rust::GlobalMiMalloc = mimalloc_rust::GlobalMiMalloc;
-
-#[cfg(all(
-    target_os = "linux",
-    target_env = "gnu",
-    any(target_arch = "x86_64", target_arch = "aarch64")
-))]
-#[global_allocator]
-static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
-
 mod analyze_deps;
 mod ast;
 mod build;
