b23bb4665fcc6ed0e4111d0276093e83d89e439d
pshu <pishu.spf@antfin.com>
2024-02-09 13:03:56 +0800

    test: ✅ add export default decl test case

---
+++ e2e/fixtures/mako.scope-hoisting.export_default_decl/expect.js
@@ -0,0 +1,12 @@
+const {
+	injectSimpleJest,
+	parseBuildResult
+} = require("../../../scripts/test-utils");
+const { files } = parseBuildResult(__dirname);
+injectSimpleJest();
+
+expect(files["index.js"]).toContain(`ROOT MODULE: ./index.js`);
+expect(files["index.js"]).toContain('__mako_module_fn_js_0');
+expect(files["index.js"]).toContain('__mako_module_class_js_0');
+
+require("./dist/index.js");
