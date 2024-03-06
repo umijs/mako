9fc368264657bbcda4a42aca6e155984977cd12a
sorrycc <sorrycc@gmail.com>
2024-03-06 14:05:02 +0800

    chore: update

--- crates/node/src/js_plugin.rs
+++ crates/node/src/js_plugin.rs
@@ -34,7 +34,8 @@ impl Plugin for JsPlugin {
             let (tx, rx) = mpsc::channel::<napi::Result<Option<LoadResult>>>();
             hook.call(
                 ReadMessage {
-                    message: param.file.pathname.clone(),
+                    // TODO: 感觉这里应该发 param.file.path
+                    message: param.file.pathname.to_string_lossy().to_string(),
                     tx,
                 },
                 threadsafe_function::ThreadsafeFunctionCallMode::Blocking,
