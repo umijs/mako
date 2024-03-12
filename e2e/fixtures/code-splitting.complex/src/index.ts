4a4956d95aaf3563d85099118504275272cd07ad
Peach <scdzwyxst@gmail.com>
2024-03-12 14:37:46 +0800

    fix: do not split root module for async chunks (#929)

--- e2e/fixtures/code-splitting.complex/src/index.ts
+++ e2e/fixtures/code-splitting.complex/src/index.ts
@@ -4,6 +4,6 @@ import Button from 'antd/es/button';
 console.log(React, Button);
 
 import('./should-be-split').then((m) => console.log(m));
-import('./should-be-common').then((m) => console.log(m));
+import('./should-not-be-common').then((m) => console.log(m));
 import('./should-be-merged').then((m) => console.log(m));
 import('./other-dynamic').then((m) => console.log(m));
