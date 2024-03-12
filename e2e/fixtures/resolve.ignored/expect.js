fc2c102c32f73de76fe412f9f2a0adbeab0a1a38
xusd320 <xusd320@gmail.com>
2024-03-12 17:29:47 +0800

    test: update e2e resolve.ignored

--- e2e/fixtures/resolve.ignored/expect.js
+++ e2e/fixtures/resolve.ignored/expect.js
@@ -2,10 +2,5 @@ const assert = require("assert");
 const { parseBuildResult } = require("../../../scripts/test-utils");
 const { distDir } = parseBuildResult(__dirname);
 
-const ret = require(path.join(distDir, 'index.js'));
+require(path.join(distDir, 'index.js'));
 
-assert.deepEqual(
-  ret,
-  {},
-  `ignored module should compile to empty es module`
-);
--- e2e/fixtures/resolve.ignored/src/index.tsx
+++ e2e/fixtures/resolve.ignored/src/index.tsx
@@ -1,2 +1,8 @@
-import * as i from "ignored";
-console.log(i);
+import assert from "assert";
+import * as import_wildcard from "ignored";
+import { some as import_partial } from "ignored";
+import import_default from "ignored";
+
+assert.deepEqual(import_wildcard, {}, `ignored module should compile to empty es module`)
+assert.equal(import_partial, undefined, `ignored module should not export anything`)
+assert.equal(import_default, undefined, `ignored module should not have a valued export default`)
