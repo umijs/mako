8cc33c3142332cbec92eeb7a838647dfc2c96555
xusd320 <xusd320@gmail.com>
2024-03-12 18:15:38 +0800

    fix: ignored module should be compiled to empty es module (#946)
    * fix: ignored module should be compiled to empty es module
    
    * test: update e2e resolve.ignored
    
    * test: update e2e resolve.ignored

--- e2e/fixtures/resolve.ignored/expect.js
+++ e2e/fixtures/resolve.ignored/expect.js
@@ -1,12 +1,6 @@
-const assert = require("assert");
-const { parseBuildResult } = require("../../../scripts/test-utils");
-const { files } = parseBuildResult(__dirname);
+const { parseBuildResult, injectSimpleJest } = require("../../../scripts/test-utils");
+const { distDir } = parseBuildResult(__dirname);
 
-const content = files["index.js"];
+injectSimpleJest()
+require(path.join(distDir, 'index.js'));
 
-assert(
-  content.includes(
-    `/*./node_modules/ignored/index.js*/ "node_modules/ignored/index.js": function(module, exports, __mako_require__) {}`
-  ),
-  `ignored module should compile to empty module`
-);
