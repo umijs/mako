ed6dbd7ec38656235b0930a8aeca0215232da4e3
guiyang <xiaodongfang.fxd@digital-engine.com>
2024-03-13 21:02:05 +0800

    feat: 改变代码逻辑，使用hygiene_with_config 修改重复标识符

--- crates/mako/src/visitors/mako_default_react_component.rs
+++ crates/mako/src/visitors/mako_default_react_component.rs
@@ -1,58 +1,20 @@
-use mako_core::regex::Regex;
 use mako_core::swc_atoms::JsWord;
-use mako_core::swc_common::sync::Lrc;
-use mako_core::swc_common::{FileName, SourceMap};
 use mako_core::swc_ecma_ast::*;
 use mako_core::swc_ecma_visit::VisitMut;
-use swc_core::common::Spanned;
 
-pub struct MakoDefaultReactComponent {
-    js_word_indents: Vec<JsWord>,
-    cm: Lrc<SourceMap>,
-}
+pub struct MakoDefaultReactComponent {}
 impl MakoDefaultReactComponent {
     #[allow(dead_code)]
-    pub fn new(cm: Lrc<SourceMap>) -> Self {
-        Self {
-            js_word_indents: vec![],
-            cm,
-        }
+    pub fn new() -> Self {
+        Self {}
     }
 }
 impl VisitMut for MakoDefaultReactComponent {
-    fn visit_mut_ident(&mut self, ident: &mut Ident) {
-        if !self.js_word_indents.contains(&ident.sym) {
-            self.js_word_indents.push(ident.sym.clone());
-        }
-    }
-    fn visit_mut_module_decl(&mut self, decl: &mut ModuleDecl) {
-        if let FileName::Real(path) = &self.cm.lookup_char_pos(decl.span().lo).file.name {
-            let str = path.to_str().unwrap();
-            let extension = path.extension().unwrap();
-            let hidden_file_reg = Regex::new(r"(^|/)\.[^/.]").unwrap();
-            if (extension != "jsx" && extension != "tsx")
-                || str.contains("node_modules")
-                || hidden_file_reg.is_match(str)
-            {
-                return;
-            }
-        } else {
-            return;
-        }
-        if let ModuleDecl::ExportDefaultDecl(default_expr) = decl {
-            if let DefaultDecl::Fn(fn_expr) = &mut default_expr.decl {
-                if fn_expr.ident.is_none() {
-                    let mut counter = 1;
-                    let js_str = "Component$$";
-                    let mut default_name = JsWord::from(format!("{}{}", js_str, counter));
-                    while self.js_word_indents.contains(&default_name) {
-                        default_name = JsWord::from(format!("{}{}", js_str, counter));
-                        counter += 1;
-                    }
-                    let ident = Ident::new(default_name, fn_expr.function.span);
-                    fn_expr.ident = Some(ident.clone());
-                    self.js_word_indents.push(ident.sym);
-                }
+    fn visit_mut_export_default_decl(&mut self, decl: &mut ExportDefaultDecl) {
+        if let DefaultDecl::Fn(fn_expr) = &mut decl.decl {
+            if fn_expr.ident.is_none() {
+                let ident = Ident::new(JsWord::from("Component$$"), fn_expr.function.span);
+                fn_expr.ident = Some(ident.clone());
             }
         }
     }
@@ -60,65 +22,39 @@ impl VisitMut for MakoDefaultReactComponent {
 
 #[cfg(test)]
 mod tests {
-    use mako_core::swc_common::errors::{ColorConfig, Handler};
     use mako_core::swc_common::sync::Lrc;
-    use mako_core::swc_common::{FileName, SourceMap};
+    use mako_core::swc_common::SourceMap;
     use mako_core::swc_ecma_ast::*;
     use mako_core::swc_ecma_codegen::text_writer::JsWriter;
     use mako_core::swc_ecma_codegen::{Config, Emitter};
-    use mako_core::swc_ecma_parser::lexer::Lexer;
-    use mako_core::swc_ecma_parser::{Parser, StringInput, Syntax};
+    use mako_core::swc_ecma_transforms::hygiene::hygiene_with_config;
     use mako_core::swc_ecma_visit::VisitMutWith;
     use swc_core::common::comments::NoopComments;
+    use swc_core::common::GLOBALS;
     use swc_core::ecma::transforms::testing::test;
 
+    use crate::ast_2::tests::TestUtils;
     #[test]
     fn test_normal() {
-        assert_eq!(
-            run("export default function(){} let a=1;let b=2").as_str(),
-            "export default function Component$$1() {}\nlet a = 1;\nlet b = 2;\n"
-        );
+        assert!(run1(r#"export default function(){}"#).contains("Component$$"));
+        assert!(run1(r#"export default function(){} let Component$$ = 1"#).contains("Component$$1"));
+        assert!(run1(r#"let Component$$ = 1; export default function(){}"#).contains("Component$$1"));
     }
-    fn run(js_code: &str) -> String {
-        let cm: Lrc<SourceMap> = Default::default();
-        let handler = Handler::with_tty_emitter(ColorConfig::Auto, false, false, Some(cm.clone()));
-
-        let fm: Lrc<swc_core::common::SourceFile> = cm.new_source_file(
-            FileName::Real("path/to/your/file.jsx".into()),
-            js_code.into(),
-        );
-        let lexer = Lexer::new(
-            // We want to parse ecmascript
-            Syntax::Es(Default::default()),
-            // EsVersion defaults to es5
-            Default::default(),
-            StringInput::from(&*fm),
-            None,
-        );
-
-        let mut parser = Parser::new_from(lexer);
-
-        for e in parser.take_errors() {
-            e.into_diagnostic(&handler).emit();
-        }
-        // GLOBALS.set(&Globals::new(), || {
-        let mut module = parser
-            .parse_module()
-            .map_err(|e| {
-                // Unrecoverable fatal error occurred
-                e.into_diagnostic(&handler).emit()
-            })
-            .expect("failed to parser module");
-        // 创建 Emitter 实例
-
-        // 获取生成的字符串
-        // let mark = module.span.ctxt().outer();
-        let mut default = super::MakoDefaultReactComponent::new(cm);
-        module.visit_mut_children_with(&mut default);
-        module_to_string(&module)
-        // })
+    fn run1(js_code: &str) -> String {
+        let mut test_utils = TestUtils::gen_js_ast(js_code.to_string());
+        let ast = test_utils.ast.js_mut();
+        let mut analyzer = super::MakoDefaultReactComponent::new();
+        GLOBALS.set(&test_utils.context.meta.script.globals, || {
+            ast.ast.visit_mut_children_with(&mut analyzer);
+        });
+        // 使用 hygiene 模块修改重复的变量名
+        let config = mako_core::swc_ecma_transforms::hygiene::Config {
+            ..Default::default()
+        };
+        let mut hygiene_transform = hygiene_with_config(config);
+        ast.ast.visit_mut_with(&mut hygiene_transform);
+        module_to_string(&ast.ast)
     }
-
     pub fn module_to_string(module: &Module) -> String {
         // 初始化 SourceMap 和 Handler，它们对于 Emitter 是必要的
 
