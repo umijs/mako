a6f4f12a528b3f13850ba3208176d770ae1bdf9d
yuyuehui.yyh <yuyuehui.yyh@digital-engine.com>
2024-03-19 19:06:43 +0800

    feat:✨remove unused specifier

--- e2e/fixtures/tree-shaking.side-effects-true.unused-specifier/expect.js
+++ e2e/fixtures/tree-shaking.side-effects-true.unused-specifier/expect.js
@@ -1,23 +1,14 @@
 const assert = require("assert");
-const { parseBuildResult } = require("../../../scripts/test-utils");
+const { parseBuildResult, moduleReg } = require("../../../scripts/test-utils");
 const { files } = parseBuildResult(__dirname);
 
 const content = files["index.js"];
 
-assert(
-    content.includes("/a.js") && 
-        !content.includes("_a"),
-    `should remove default specifier`
-);
-
-assert(
-    content.includes("/b.js") && 
-        !content.includes("_b"),
-    `should remove namespace specifier`
-);
-
-assert(
-    content.includes("/c.js") && 
-        !content.includes("_c"),
-    `should remove namespace specifier`
+assert.match(
+  content,
+  moduleReg(
+    "src/index.js",
+    `__mako_require__\\("src/a.js"\\);[\\s\\S]*__mako_require__\\("src/b.js"\\);[\\s\\S]*__mako_require__\\("src/c.js"\\);`
+  ),
+  `should remove namespace specifier`
 );
