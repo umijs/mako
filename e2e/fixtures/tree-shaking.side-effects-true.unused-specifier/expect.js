e27ab1d7857de4c5eba7b4d35c36dfdfcbb5d1d9
yuyuehui.yyh <yuyuehui.yyh@digital-engine.com>
2024-03-19 21:11:39 +0800

    test:✅refactor e2e test

--- e2e/fixtures/tree-shaking.side-effects-true.unused-specifier/expect.js
+++ e2e/fixtures/tree-shaking.side-effects-true.unused-specifier/expect.js
@@ -2,13 +2,33 @@ const assert = require("assert");
 const { parseBuildResult, moduleReg } = require("../../../scripts/test-utils");
 const { files } = parseBuildResult(__dirname);
 
-const content = files["index.js"];
+const contentA = files["entry-a.js"];
+const contentB = files["entry-b.js"];
+const contentC = files["entry-c.js"];
 
 assert.match(
-  content,
+  contentA,
   moduleReg(
-    "src/index.js",
-    `__mako_require__\\("src/a.js"\\);[\\s]*__mako_require__\\("src/b.js"\\);[\\s]*__mako_require__\\("src/c.js"\\);`
+    "src/entry-a.js",
+    `__mako_require__\\("src/a.js"\\);`
+  ),
+  `should remove default specifier`
+);
+
+assert.match(
+  contentB,
+  moduleReg(
+    "src/entry-b.js",
+    `__mako_require__\\("src/b.js"\\);`
   ),
   `should remove namespace specifier`
+);
+
+assert.match(
+  contentC,
+  moduleReg(
+    "src/entry-c.js",
+    `__mako_require__\\("src/c.js"\\);`
+  ),
+  `should remove named specifier`
 );
--- e2e/fixtures/tree-shaking.side-effects-true.unused-specifier/src/index.js
+++
@@ -1,5 +0,0 @@
-import a from './a.js';
-import * as b from './b.js';
-import { c, cc as ccc } from './c.js';
-
-console.log('this is entry');
