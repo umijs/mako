0b974d72b1144753993fd0f82f608fb913bbc78c
xusd320 <xusd320@gmail.com>
2024-04-01 14:14:37 +0800

    fix: entry chunk hash (#1003)

--- crates/mako/src/swc_helpers.rs
+++ crates/mako/src/swc_helpers.rs
@@ -1,21 +1,21 @@
-use std::collections::HashSet;
+use mako_core::indexmap::IndexSet;
 
 pub struct SwcHelpers {
-    pub helpers: HashSet<String>,
+    pub helpers: IndexSet<String>,
 }
 
 impl SwcHelpers {
-    pub fn new(helpers: Option<HashSet<String>>) -> Self {
+    pub fn new(helpers: Option<IndexSet<String>>) -> Self {
         let helpers = if let Some(helpers) = helpers {
             helpers
         } else {
-            HashSet::new()
+            IndexSet::new()
         };
         Self { helpers }
     }
 
-    pub fn full_helpers() -> HashSet<String> {
-        let mut helpers = HashSet::new();
+    pub fn full_helpers() -> IndexSet<String> {
+        let mut helpers = IndexSet::new();
         helpers.insert("@swc/helpers/_/_interop_require_default".into());
         helpers.insert("@swc/helpers/_/_interop_require_wildcard".into());
         helpers.insert("@swc/helpers/_/_export_star".into());
