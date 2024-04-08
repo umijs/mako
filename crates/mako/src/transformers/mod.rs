6dc4458329d7361c6d81518df679dd670e28b16f
chencheng (云谦) <sorrycc@gmail.com>
2024-04-08 17:33:41 +0800

    refact: move react to visitors (#1037)

--- crates/mako/src/transformers/mod.rs
+++ crates/mako/src/transformers/mod.rs
@@ -6,7 +6,6 @@ pub mod transform_meta_url_replacer;
 pub mod transform_optimize_define_utils;
 // pub mod transform_optimize_package_imports;
 pub mod transform_px2rem;
-pub mod transform_react;
 pub mod utils;
 
 #[cfg(test)]
