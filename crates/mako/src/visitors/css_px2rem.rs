8fa4dfe1942e69778d3f78572a8fa3cef1dbfb32
谦男 <haolongli.lhl@digital-engine.com>
2024-04-08 20:51:23 +0800

    fix: 完成对应功能

--- crates/mako/src/visitors/css_px2rem.rs
+++ crates/mako/src/visitors/css_px2rem.rs
@@ -1,7 +1,9 @@
 use std::sync::Arc;
 
+use mako_core::regex::Regex;
 use mako_core::swc_css_ast::{self, Length, Token};
 use mako_core::swc_css_visit::{VisitMut, VisitMutWith};
+use swc_core::css::ast::{CompoundSelector, SubclassSelector};
 
 use crate::compiler::Context;
 
@@ -18,38 +20,46 @@ pub struct Px2Rem {
 
 impl Px2Rem {
     fn should_transform(&self) -> bool {
-        if let Some(current_decl) = &self.current_decl {
-            let is_in_whitelist = self
+        let mut is_in_is_in_whitelist_and_is_in_blacklist = true;
+        is_in_is_in_whitelist_and_is_in_blacklist = if let Some(current_decl) = &self.current_decl {
+            let px2rem_config = self
                 .context
                 .config
                 .px2rem
                 .as_ref()
-                .unwrap()
-                .prop_white_list
-                .is_empty()
-                || self
-                    .context
-                    .config
-                    .px2rem
-                    .as_ref()
-                    .unwrap()
-                    .prop_white_list
-                    .contains(current_decl);
-            let is_in_blacklist = self
-                .context
-                .config
-                .px2rem
-                .as_ref()
-                .unwrap()
-                .prop_black_list
-                .contains(current_decl);
-            return is_in_whitelist && !is_in_blacklist;
+                .expect("px2rem config should exist");
+            let is_in_whitelist = px2rem_config.prop_white_list.is_empty()
+                || px2rem_config.prop_white_list.contains(current_decl);
+
+            let is_in_blacklist = px2rem_config.prop_black_list.contains(current_decl);
+
+            is_in_whitelist && !is_in_blacklist
+        } else {
+            false // 或者选择一个合适的默认值
+        };
+        let mut is_in_is_select_black = true;
+        let selector_black_list = &self
+            .context
+            .as_ref()
+            .config
+            .px2rem
+            .as_ref()
+            .unwrap()
+            .selector_black_list;
+        for reg_String in selector_black_list {
+            let re = Regex::new(reg_String).unwrap();
+            is_in_is_select_black =
+                !re.is_match(self.current_selector.as_ref().unwrap_or(&String::from("")));
+            if !is_in_is_select_black {
+                break;
+            }
         }
-        true
+        is_in_is_in_whitelist_and_is_in_blacklist && is_in_is_select_black
     }
 }
 
 impl VisitMut for Px2Rem {
+    // 应该是这里决定的? 传入不同的类型转换为不同的
     fn visit_mut_declaration(&mut self, n: &mut swc_css_ast::Declaration) {
         self.current_decl = match n.name {
             swc_css_ast::DeclarationName::Ident(ref ident) => Some(ident.value.to_string()),
@@ -59,6 +69,36 @@ impl VisitMut for Px2Rem {
         };
         n.visit_mut_children_with(self);
         self.current_decl = None;
+    }
+    fn visit_mut_compound_selector(&mut self, n: &mut CompoundSelector) {
+        println!("CompoundSelectorLHL=={:?}", n);
+        if !n.subclass_selectors.is_empty() {
+            // 假设我们只关心第一个 subclass_selector，因此获取第一个元素
+
+            // 使用匹配来处理不同类型的 SubclassSelector
+            self.current_selector = match n.subclass_selectors.first().unwrap() {
+                SubclassSelector::Class(class_selector) => {
+                    Some(class_selector.text.value.clone().to_string())
+                }
+                // ... 处理 SubclassSelector 的其他变体 ...
+                _ => None,
+            };
+        } else if let Some(type_selector) = &n.type_selector {
+            self.current_selector = Some(
+                type_selector
+                    .clone()
+                    .tag_name()
+                    .unwrap()
+                    .name
+                    .value
+                    .value
+                    .to_string(),
+            )
+        } else {
+            self.current_selector = Some(String::from(""));
+        }
+        // self.current_selector = match &n { CompoundSelector{..}=>Some() }
+        n.visit_mut_children_with(self);
     }
     fn visit_mut_length(&mut self, n: &mut Length) {
         if n.unit.value.to_string() == "px" && self.should_transform() {
