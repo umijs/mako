080fece6ebe3b50c1e95f1d68eedfc2e18b7a407
谦男 <haolongli.lhl@digital-engine.com>
2024-04-10 22:43:53 +0800

    fix: 修复大量初始化 Regex 初始化&修复 curr_select 赋值问题

--- crates/mako/src/visitors/css_px2rem.rs
+++ crates/mako/src/visitors/css_px2rem.rs
@@ -1,7 +1,7 @@
 use mako_core::regex::Regex;
 use mako_core::swc_css_ast::{self, Length, Token};
 use mako_core::swc_css_visit::{VisitMut, VisitMutWith};
-use swc_core::css::ast::{CompoundSelector, SubclassSelector};
+use swc_core::css::ast::{Combinator, ComplexSelector, CompoundSelector, SubclassSelector};
 
 use crate::config::Px2RemConfig;
 
@@ -11,16 +11,53 @@ pub(crate) fn default_root() -> f64 {
 
 pub struct Px2Rem {
     pub config: Px2RemConfig,
-    pub current_decl: Option<String>,
-    pub current_selector: Option<String>,
+    current_decl: Option<String>,
+    current_selector: Option<String>,
+    select_black_regx: Vec<Regex>,
+    select_white_regx: Vec<Regex>,
+}
+
+fn match_str_or_regex_based_on_chars(pattern: &str, input: &str, regx_arr: &[Regex]) -> bool {
+    if pattern == input {
+        // 第一个参数和第二个参数全等
+        true
+    } else if pattern.contains('*')
+        || pattern.contains('\\')
+        || pattern.contains('(')
+        || pattern.contains(')')
+        || pattern.contains('^')
+        || pattern.contains('$')
+    {
+        // 参数包含特定的正则表达式字符，尝试将第一个参数作为正则表达式
+        regx_arr.iter().any(|regx| regx.is_match(input))
+    } else {
+        // 如果不包含特定字符且不全等，返回 false
+        false
+    }
 }
 
 impl Px2Rem {
     pub fn new(config: Px2RemConfig) -> Self {
+        let mut select_black_regx = vec![];
+        let mut select_white_regx = vec![];
+        if !config.selector_black_list.is_empty() {
+            select_black_regx = config
+                .selector_black_list
+                .iter()
+                .filter_map(|pattern| Regex::new(pattern).ok()) // 这里 `ok()` 会将 `Result` 转换为 `Option`
+                .collect();
+            select_white_regx = config
+                .selector_white_list
+                .iter()
+                .filter_map(|pattern| Regex::new(pattern).ok()) // 这里 `ok()` 会将 `Result` 转换为 `Option`
+                .collect();
+        }
         Self {
             config,
             current_decl: None,
             current_selector: None,
+            select_black_regx,
+            select_white_regx,
         }
     }
 
@@ -35,15 +72,17 @@ impl Px2Rem {
         };
         let is_selector_valid = if let Some(selector) = &self.current_selector {
             let is_whitelist_empty = self.config.selector_white_list.is_empty();
+            // 判断是否是字符串还是正则匹配
+
             let is_in_whitelist = self.config.selector_white_list.iter().any(|pattern| {
-                let re = Regex::new(pattern).unwrap();
-                re.is_match(selector)
+                match_str_or_regex_based_on_chars(pattern, selector, &self.select_white_regx)
             });
+
             let is_in_blacklist = self.config.selector_black_list.iter().any(|pattern| {
                 // TODO: should have performance issues, need benchmark
-                let re = Regex::new(pattern).unwrap();
-                re.is_match(selector)
+                match_str_or_regex_based_on_chars(pattern, selector, &self.select_black_regx)
             });
+
             (is_whitelist_empty || is_in_whitelist) && !is_in_blacklist
         } else {
             true
@@ -64,12 +103,37 @@ impl VisitMut for Px2Rem {
         self.current_decl = None;
     }
 
+    fn visit_mut_complex_selector(&mut self, n: &mut ComplexSelector) {
+        // 判断是否有值
+        if let Some(ref mut currselect) = self.current_selector {
+            currselect.push(",".parse().unwrap())
+        } else {
+            self.current_selector = None
+        }
+
+        n.visit_mut_children_with(self);
+    }
+    fn visit_mut_combinator(&mut self, n: &mut Combinator) {
+        let value = n.value;
+
+        if let Some(ref mut current_sele) = self.current_selector {
+            current_sele.push(value.to_string().parse().unwrap());
+        }
+
+        n.visit_mut_children_with(self);
+    }
     fn visit_mut_compound_selector(&mut self, n: &mut CompoundSelector) {
+        let mut cur_select = None;
         if !n.subclass_selectors.is_empty() {
             // 假设我们只关心第一个 subclass_selector，因此获取第一个元素
             // 使用匹配来处理不同类型的 SubclassSelector
-            self.current_selector = match n.subclass_selectors.first().unwrap() {
+
+            //累加
+
+            match n.subclass_selectors.first().unwrap() {
                 SubclassSelector::Class(class_selector) => {
+                    cur_select = Some(format!("{}{}", ".", class_selector.text.value.clone()));
+
                     Some(class_selector.text.value.clone().to_string())
                 }
                 // ... 处理 SubclassSelector 的其他变体 ...
@@ -77,11 +141,17 @@ impl VisitMut for Px2Rem {
             };
         } else if let Some(type_selector) = &n.type_selector {
             if let Some(name) = type_selector.clone().tag_name() {
-                self.current_selector = Some(name.name.value.value.to_string());
+                cur_select = Some(name.name.value.value.to_string());
             }
         } else {
             self.current_selector = None;
         }
+        if let Some(ref mut cur_value) = self.current_selector {
+            cur_value.push_str(&cur_select.unwrap_or("".to_string()));
+        } else {
+            self.current_selector = cur_select;
+        }
+
         n.visit_mut_children_with(self);
     }
 
@@ -91,6 +161,7 @@ impl VisitMut for Px2Rem {
             n.value.raw = None;
             n.unit.value = "rem".into();
         }
+        self.current_selector = None;
         n.visit_mut_children_with(self);
     }
 
@@ -183,7 +254,7 @@ mod tests {
             run(
                 r#".a{width:100px;}.b{width:100px;}"#,
                 Px2RemConfig {
-                    selector_white_list: vec!["a".to_string()],
+                    selector_white_list: vec![".a".to_string()],
                     selector_black_list: vec![],
                     ..Default::default()
                 }
@@ -198,7 +269,7 @@ mod tests {
             run(
                 r#".a{width:100px;}.b{width:100px;}"#,
                 Px2RemConfig {
-                    selector_black_list: vec!["a".to_string()],
+                    selector_black_list: vec![".a".to_string()],
                     ..Default::default()
                 }
             ),
@@ -208,18 +279,18 @@ mod tests {
             run(
                 r#".a{width:100px;}.ac{width:100px;}.b{width:100px;}"#,
                 Px2RemConfig {
-                    selector_black_list: vec!["a".to_string()],
+                    selector_black_list: vec![".a".to_string()],
                     ..Default::default()
                 }
             ),
             // .ac is matched by "a"
-            r#".a{width:100px}.ac{width:100px}.b{width:1rem}"#
+            r#".a{width:100px}.ac{width:1rem}.b{width:1rem}"#
         );
         assert_eq!(
             run(
                 r#".a{width:100px;}.ac{width:100px;}.b{width:100px;}"#,
                 Px2RemConfig {
-                    selector_black_list: vec!["^a$".to_string()],
+                    selector_black_list: vec!["^.a$".to_string()],
                     ..Default::default()
                 }
             ),
@@ -234,13 +305,14 @@ mod tests {
     fn test_multi_selectors_whitelist() {
         assert_eq!(
             run(
-                r#".a,.b{width:100px;}"#,
+                r#".a .h,.b,.c>.g .kk,h1 div{width:100px;}"#,
                 Px2RemConfig {
-                    selector_white_list: vec!["b".to_string()],
+                    selector_white_list: vec![".a .h,.b,.c>.g .kk,h1 div".to_string()],
+                    selector_black_list: vec![".a .h,.b,.c>.g .kk,h1 ".to_string()],
                     ..Default::default()
                 }
             ),
-            r#".a,.b{width:100px}"#
+            r#".a .h,.b,.c>.g .kk,h1 div{width:1rem}"#
         );
     }
 
