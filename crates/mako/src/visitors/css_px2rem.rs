62b62665f467f35c48b03831369b848fe46b0bd1
LovePlayCode <51304857+LovePlayCode@users.noreply.github.com>
2024-04-12 12:25:11 +0800

    feat: improve px2rem, support selector_black_list and selector_white_list (#1043)
    * feat: 完成 select_black&select_white
    
    * fix: 修复 aci 问题
    
    * chore: code style
    
    * chore: code style
    
    * chore: add multiple selectors testcase
    
    * fix: 修复大量初始化 Regex 初始化&修复 curr_select 赋值问题
    
    * chore: improve
    
    * chore: update
    
    * chore: rename
    
    ---------
    
    Co-authored-by: 谦男 <haolongli.lhl@digital-engine.com>
    Co-authored-by: sorrycc <sorrycc@gmail.com>

--- crates/mako/src/visitors/css_px2rem.rs
+++ crates/mako/src/visitors/css_px2rem.rs
@@ -1,5 +1,9 @@
-use mako_core::swc_css_ast::{self, Length, Token};
+use mako_core::regex::Regex;
+use mako_core::swc_css_ast::{
+    self, Combinator, CombinatorValue, ComplexSelectorChildren, Length, Token, TypeSelector,
+};
 use mako_core::swc_css_visit::{VisitMut, VisitMutWith};
+use swc_core::css::ast::{ComplexSelector, CompoundSelector, SubclassSelector};
 
 use crate::config::Px2RemConfig;
 
@@ -9,20 +13,52 @@ pub(crate) fn default_root() -> f64 {
 
 pub struct Px2Rem {
     pub config: Px2RemConfig,
-    pub current_decl: Option<String>,
-    // TODO: support selector
-    pub current_selector: Option<String>,
+    current_decl: Option<String>,
+    current_selectors: Vec<String>,
+    selector_blacklist: Vec<Regex>,
+    selector_whitelist: Vec<Regex>,
 }
 
 impl Px2Rem {
-    fn should_transform(&self) -> bool {
-        if let Some(current_decl) = &self.current_decl {
-            let is_whitelist_empty = self.config.prop_white_list.is_empty();
-            let is_in_whitelist = self.config.prop_white_list.contains(current_decl);
-            let is_in_blacklist = self.config.prop_black_list.contains(current_decl);
-            return (is_whitelist_empty || is_in_whitelist) && !is_in_blacklist;
+    pub fn new(config: Px2RemConfig) -> Self {
+        let selector_blacklist = parse_patterns(&config.selector_blacklist);
+        let selector_whitelist = parse_patterns(&config.selector_whitelist);
+        Self {
+            config,
+            current_decl: None,
+            current_selectors: vec![],
+            selector_blacklist,
+            selector_whitelist,
         }
-        true
+    }
+
+    fn should_transform(&self) -> bool {
+        let is_prop_valid = if let Some(decl) = &self.current_decl {
+            let is_whitelist_empty = self.config.prop_whitelist.is_empty();
+            let is_in_whitelist = self.config.prop_whitelist.contains(decl);
+            let is_in_blacklist = self.config.prop_blacklist.contains(decl);
+            (is_whitelist_empty || is_in_whitelist) && !is_in_blacklist
+        } else {
+            true
+        };
+        let is_selector_valid = {
+            if self.current_selectors.is_empty() {
+                return true;
+            }
+            let is_whitelist_empty = self.config.selector_whitelist.is_empty();
+            let is_all_in_whitelist = self.current_selectors.iter().all(|selector| {
+                self.selector_whitelist
+                    .iter()
+                    .any(|regx| regx.is_match(selector))
+            });
+            let is_any_in_blacklist = self.current_selectors.iter().any(|selector| {
+                self.selector_blacklist
+                    .iter()
+                    .any(|regx| regx.is_match(selector))
+            });
+            (is_whitelist_empty || is_all_in_whitelist) && !is_any_in_blacklist
+        };
+        is_prop_valid && is_selector_valid
     }
 }
 
@@ -36,6 +72,17 @@ impl VisitMut for Px2Rem {
         };
         n.visit_mut_children_with(self);
         self.current_decl = None;
+    }
+
+    fn visit_mut_qualified_rule(&mut self, n: &mut swc_css_ast::QualifiedRule) {
+        self.current_selectors = vec![];
+        n.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_complex_selector(&mut self, n: &mut ComplexSelector) {
+        let selector = parse_complex_selector(n);
+        self.current_selectors.push(selector);
+        n.visit_mut_children_with(self);
     }
 
     fn visit_mut_length(&mut self, n: &mut Length) {
@@ -61,10 +108,89 @@ impl VisitMut for Px2Rem {
     }
 }
 
+fn parse_patterns(patterns: &[String]) -> Vec<Regex> {
+    patterns
+        .iter()
+        .map(|pattern| {
+            let pattern = if contains_magic_chars(pattern) {
+                pattern.to_string()
+            } else {
+                format!("^{}$", pattern)
+            };
+            Regex::new(pattern.as_str()).unwrap()
+        })
+        .collect()
+}
+
+fn contains_magic_chars(pattern: &str) -> bool {
+    pattern.contains('*')
+        || pattern.contains('\\')
+        || pattern.contains('(')
+        || pattern.contains(')')
+        || pattern.contains('^')
+        || pattern.contains('$')
+}
+
+fn parse_combinator(combinator: &Combinator) -> String {
+    match combinator.value {
+        CombinatorValue::Descendant => " ".to_string(),
+        CombinatorValue::Child => ">".to_string(),
+        CombinatorValue::NextSibling => "+".to_string(),
+        CombinatorValue::LaterSibling => "~".to_string(),
+        CombinatorValue::Column => "||".to_string(),
+    }
+}
+
+fn parse_compound_selector(selector: &CompoundSelector) -> String {
+    let mut result = String::new();
+    // TODO: support selector.nesting_selector
+    if let Some(type_selector) = &selector.type_selector {
+        let type_selector = type_selector.as_ref();
+        match type_selector {
+            TypeSelector::TagName(tag_name_selector) => {
+                result.push_str(tag_name_selector.name.value.value.as_ref());
+            }
+            TypeSelector::Universal(_) => {
+                result.push('*');
+            }
+        }
+    }
+    for subclass_selector in &selector.subclass_selectors {
+        match subclass_selector {
+            SubclassSelector::Id(id) => {
+                result.push_str(&format!("#{}", id.text.value));
+            }
+            SubclassSelector::Class(class) => {
+                result.push_str(&format!(".{}", class.text.value));
+            }
+            _ => {
+                // TODO: support more subclass selectors
+            }
+        }
+    }
+    result
+}
+
+fn parse_complex_selector(selector: &ComplexSelector) -> String {
+    let mut result = String::new();
+    for child in &selector.children {
+        match child {
+            ComplexSelectorChildren::CompoundSelector(compound_selector) => {
+                result.push_str(parse_compound_selector(compound_selector).as_str());
+            }
+            ComplexSelectorChildren::Combinator(combinator, ..) => {
+                result.push_str(parse_combinator(combinator).as_str());
+            }
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use mako_core::swc_css_visit::VisitMutWith;
 
+    use super::Px2Rem;
     use crate::ast::tests::TestUtils;
     use crate::config::Px2RemConfig;
 
@@ -101,12 +227,12 @@ mod tests {
     }
 
     #[test]
-    fn test_blacklist() {
+    fn test_prop_blacklist() {
         assert_eq!(
             run(
                 r#".a{width:100px;height:100px;}"#,
                 Px2RemConfig {
-                    prop_black_list: vec!["width".to_string()],
+                    prop_blacklist: vec!["width".to_string()],
                     ..Default::default()
                 }
             ),
@@ -115,17 +241,190 @@ mod tests {
     }
 
     #[test]
-    fn test_whitelist() {
+    fn test_prop_whitelist() {
         assert_eq!(
             run(
                 r#".a{width:100px;height:100px;}"#,
                 Px2RemConfig {
-                    prop_white_list: vec!["width".to_string()],
-                    prop_black_list: vec![],
+                    prop_whitelist: vec!["width".to_string()],
+                    prop_blacklist: vec![],
                     ..Default::default()
                 }
             ),
             r#".a{width:1rem;height:100px}"#
+        );
+    }
+
+    #[test]
+    fn test_selector_whitelist() {
+        assert_eq!(
+            run(
+                r#".a{width:100px;}.b{width:100px;}"#,
+                Px2RemConfig {
+                    selector_whitelist: vec![".a".to_string()],
+                    selector_blacklist: vec![],
+                    ..Default::default()
+                }
+            ),
+            r#".a{width:1rem}.b{width:100px}"#
+        );
+    }
+
+    #[test]
+    fn test_selector_blacklist() {
+        assert_eq!(
+            run(
+                r#".a{width:100px;}.b{width:100px;}"#,
+                Px2RemConfig {
+                    selector_blacklist: vec![".a".to_string()],
+                    ..Default::default()
+                }
+            ),
+            r#".a{width:100px}.b{width:1rem}"#
+        );
+    }
+
+    #[test]
+    fn test_selector_blacklist_exact_match() {
+        assert_eq!(
+            run(
+                r#".a{width:100px;}.ac{width:100px;}.b{width:100px;}"#,
+                Px2RemConfig {
+                    selector_blacklist: vec![".a".to_string()],
+                    ..Default::default()
+                }
+            ),
+            // .ac should not be matched by .a
+            r#".a{width:100px}.ac{width:1rem}.b{width:1rem}"#
+        );
+        assert_eq!(
+            run(
+                r#".a{width:100px;}.ac{width:100px;}.b{width:100px;}"#,
+                Px2RemConfig {
+                    selector_blacklist: vec!["^.a$".to_string()],
+                    ..Default::default()
+                }
+            ),
+            r#".a{width:100px}.ac{width:1rem}.b{width:1rem}"#
+        );
+    }
+
+    #[test]
+    fn test_selector_blacklist_id() {
+        assert_eq!(
+            run(
+                r#"#a{width:100px;}"#,
+                Px2RemConfig {
+                    selector_blacklist: vec!["#a".to_string()],
+                    ..Default::default()
+                }
+            ),
+            r#"#a{width:100px}"#
+        );
+    }
+
+    #[test]
+    fn test_selector_blacklist_tagname() {
+        assert_eq!(
+            run(
+                r#"div{width:100px;}"#,
+                Px2RemConfig {
+                    selector_blacklist: vec!["div".to_string()],
+                    ..Default::default()
+                }
+            ),
+            r#"div{width:100px}"#
+        );
+    }
+
+    #[test]
+    fn test_selector_blacklist_unique() {
+        assert_eq!(
+            run(
+                r#"div *{width:100px;}"#,
+                Px2RemConfig {
+                    selector_blacklist: vec!["div *".to_string()],
+                    ..Default::default()
+                }
+            ),
+            r#"div *{width:100px}"#
+        );
+    }
+
+    #[test]
+    fn test_selector_blacklist_multiple_classes() {
+        assert_eq!(
+            run(
+                r#".a.b{width:100px;}"#,
+                Px2RemConfig {
+                    selector_blacklist: vec![".a.b".to_string()],
+                    ..Default::default()
+                }
+            ),
+            r#".a.b{width:100px}"#
+        );
+    }
+
+    #[test]
+    fn test_selector_blacklist_child() {
+        assert_eq!(
+            run(
+                r#".a > .b{width:100px;}"#,
+                Px2RemConfig {
+                    // TODO: handle .a > .b (with space in between)
+                    selector_blacklist: vec![".a>.b".to_string()],
+                    ..Default::default()
+                }
+            ),
+            r#".a>.b{width:100px}"#
+        );
+    }
+
+    #[test]
+    fn test_multi_selectors_whitelist() {
+        assert_eq!(
+            run(
+                r#".a,.b{width:100px;}"#,
+                Px2RemConfig {
+                    selector_whitelist: vec![],
+                    selector_blacklist: vec![],
+                    ..Default::default()
+                }
+            ),
+            r#".a,.b{width:1rem}"#
+        );
+        assert_eq!(
+            run(
+                r#".a,.b{width:100px;}"#,
+                Px2RemConfig {
+                    selector_whitelist: vec![".a".to_string()],
+                    selector_blacklist: vec![],
+                    ..Default::default()
+                }
+            ),
+            r#".a,.b{width:100px}"#
+        );
+        assert_eq!(
+            run(
+                r#".a,.b{width:100px;}"#,
+                Px2RemConfig {
+                    selector_whitelist: vec![".a".to_string(), ".b".to_string()],
+                    selector_blacklist: vec![],
+                    ..Default::default()
+                }
+            ),
+            r#".a,.b{width:1rem}"#
+        );
+        assert_eq!(
+            run(
+                r#".a,.b{width:100px;}"#,
+                Px2RemConfig {
+                    selector_whitelist: vec![],
+                    selector_blacklist: vec![".a".to_string()],
+                    ..Default::default()
+                }
+            ),
+            r#".a,.b{width:100px}"#
         );
     }
 
@@ -136,11 +435,7 @@ mod tests {
     fn run(css_code: &str, config: Px2RemConfig) -> String {
         let mut test_utils = TestUtils::gen_css_ast(css_code.to_string(), true);
         let ast = test_utils.ast.css_mut();
-        let mut visitor = super::Px2Rem {
-            config,
-            current_decl: None,
-            current_selector: None,
-        };
+        let mut visitor = Px2Rem::new(config);
         ast.ast.visit_mut_with(&mut visitor);
         let code = test_utils.css_ast_to_code();
         println!("{}", code);
