f0b8ad0f48ef8ed3f2a21335654426279cbc4bec
pshu <stormslowly@gmail.com>
2024-04-15 21:54:57 +0800

    chore(binding): quick release (#1045)
    * release: @okamjs/okam@0.4.10-dev.20240410.1
    
    * chore: 🙈  add artifacts tar ignore
    
    * chore: ➕ quick release script
    
    * release: @okamjs/okam@0.4.10-dev.20240410.2
    
    * chore: ⬆️ pump version
    
    * fix: 🐛 binding.* missing error
    
    * chore: 🎨 format
    
    * refactor: 🎨 abort is abort
    
    * chore: 🔧 add release:quick script
    
    * release: @okamjs/okam@0.4.10-dev.20240410.3
    
    * chore: ⬆️ 0.4.10-dev.20240410.3
    
    * fix: 🐛 post build not executed in pnpm
    
    * refactor: 🎨 extract common uitls fn
    
    * chore: ⬆️ pnpm lock
    
    * release: @okamjs/okam@0.4.10-dev.20240412.1
    
    * fix: 🐛 wrong version fields
    
    * release: @okamjs/okam@0.4.10-dev.20240412.2
    
    * chore: ⬆️ update npnm-lock
    
    * refactor: 🎨 rename fn name

---
+++ packages/mako/scripts/quick-release.ts
@@ -0,0 +1,58 @@
+import assert from 'assert';
+import 'zx/globals';
+import {
+  ensureGitStatus,
+  loadPkg,
+  pushToGit,
+  queryNewVersion,
+  rootPkgPath,
+  setNewVersionToBundlerOkam,
+} from './utils';
+
+(async () => {
+  await run();
+})().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
+
+async function run() {
+  await ensureGitStatus();
+
+  const commitId = (await $`git rev-parse HEAD`).stdout.trim();
+  const artifactsFile = `artifacts-${commitId}.tar`;
+  const hasArtifacts = fs.existsSync(path.join(process.cwd(), artifactsFile));
+  assert(hasArtifacts, `${artifactsFile} not found in cwd`);
+
+  const nodePkgPath = rootPkgPath();
+  const nodePkg = loadPkg(nodePkgPath);
+  const { newVersion, tag, branch } = await queryNewVersion(nodePkg);
+
+  nodePkg.version = newVersion;
+  fs.writeFileSync(nodePkgPath, JSON.stringify(nodePkg, null, 2) + '\n');
+
+  await build();
+  await artifacts(artifactsFile);
+
+  await $`npm publish --tag ${tag} --access public`;
+
+  setNewVersionToBundlerOkam(nodePkg.version);
+
+  await pushToGit(nodePkg, branch);
+}
+
+async function build() {
+  await $`rm -rf ./*.node`;
+  await $`find ./npm -name '*.node' | xargs rm -f`;
+  await $`rm -rf ./dist`;
+
+  await $`pnpm run build`;
+  await $`pnpm run src:build`;
+  await $`pnpm run format`;
+}
+
+async function artifacts(artifactsFile: string) {
+  await $`rm -rf *.node`;
+  await $`tar -vxf ${artifactsFile}`;
+  await $`npm run artifacts:local`;
+}
