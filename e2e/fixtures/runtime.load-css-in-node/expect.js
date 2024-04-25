bf4511dcebe74a49ab5117a5861e5c83ecb2836f
sorrycc <sorrycc@gmail.com>
2024-04-25 18:41:24 +0800

    fix: do not load css for node

--- e2e/fixtures/runtime.load-css-in-node/expect.js
+++ e2e/fixtures/runtime.load-css-in-node/expect.js
@@ -9,3 +9,6 @@ assert(
   !content.includes(`requireModule.chunkEnsures.css`),
   `requireModule.chunkEnsures.css should not be included in the output`
 );
+
+const hasCSSInFiles = Object.keys(files).some((file) => file.endsWith(".css"));
+assert(!hasCSSInFiles, `should not have any CSS files in the output`);
