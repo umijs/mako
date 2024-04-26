d8f8f873dc3b4dc6c2e343449428e0458e030bf1
pshu <pishu.spf@antfin.com>
2024-04-27 00:52:55 +0800

    refactor: 🔥 remove duplicate code

--- crates/mako/src/analyze_deps.rs
+++ crates/mako/src/analyze_deps.rs
@@ -5,10 +5,9 @@ use mako_core::anyhow::{anyhow, Result};
 use mako_core::thiserror::Error;
 
 use crate::ast::error;
-use crate::ast::file::{File, VIRTUAL};
+use crate::ast::file::File;
 use crate::compiler::Context;
 use crate::module::{Dependency, ModuleAst};
-use crate::resolve::ResolverResource::Virtual;
 use crate::resolve::{resolve, ResolverResource};
 
 #[derive(Debug, Error)]
@@ -53,15 +52,6 @@ impl AnalyzeDeps {
         let path = Self::resolve_from(file, context.clone());
 
         for dep in deps {
-            if dep.source.starts_with(VIRTUAL) {
-                resolved_deps.push(ResolvedDep {
-                    resolver_resource: Virtual(dep.source.clone().into()),
-                    dependency: dep,
-                });
-
-                continue;
-            }
-
             let result = resolve(
                 // .
                 &path,
