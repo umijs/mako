d10554a5bd3050b49dd636ad1f4ad1dbf5c3982c
xusd320 <xusd320@gmail.com>
2024-05-10 15:39:57 +0800

    chore: update docs

--- packages/mako/src/lessLoader/index.ts
+++ packages/mako/src/lessLoader/index.ts
@@ -17,8 +17,9 @@ export interface LessLoaderOpts {
     | undefined;
   sourceMap: any;
   /**
-   * A plugin can be a file path string, or a file path string with a params object. Notice!
-   * The params object must be a plain json. We will require the plugin file to get the plugin content.
+   * A plugin can be a file path string, or a file path string with a params object.
+   * Notice! The file path should be a resolved path like require.resolve("less-plugin-clean-css"),
+   * and the params object must be a plain json. We will require the plugin file to get the plugin content.
    * If the params object been accepted, that means, the required content will be treated as a factory class of Less.Plugin,
    * we will create a plugin instance with the params object, or else, the required content will be treated as a plugin instance.
    * We do this because the less loader runs in a worker pool for speed, and a less plugin instance can't be passed to worker directly.
