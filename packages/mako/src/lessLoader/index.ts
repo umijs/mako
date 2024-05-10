0007187d05ada23e6b8292ba84b16646c5e8c219
xusd320 <xusd320@gmail.com>
2024-05-10 13:16:42 +0800

    feat: supports less plugin

--- packages/mako/src/lessLoader/index.ts
+++ packages/mako/src/lessLoader/index.ts
@@ -2,7 +2,10 @@ import url from 'url';
 import { compile, terminatePool } from './parallelLessLoader';
 
 export interface LessLoaderOpts {
-  alias: Record<string, string>;
+  /**
+   * @deprecated
+   */
+  alias?: Record<string, string>;
   modifyVars: Record<string, string>;
   math:
     | 'always'
@@ -13,6 +16,14 @@ export interface LessLoaderOpts {
     | number
     | undefined;
   sourceMap: any;
+  /**
+   * A plugin can be a file path string, or a file path string with a params object. Notice!
+   * The params object must be a plain json. We will require the plugin file to get the plugin content.
+   * If the params object been accepted, that means, the required content will be treated as a factory class of Less.Plugin,
+   * we will create a plugin instance with the params object, or else, the required content will be treated as a plugin instance.
+   * We do this because the less loader runs in a worker pool for speed, and a less plugin instance can't be passed to worker directly.
+   */
+  plugins?: (string | [string, Record<string, any>])[];
 }
 
 function lessLoader(fn: Function | null, opts: LessLoaderOpts) {
