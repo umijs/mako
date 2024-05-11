948d6f325c5daf137da9246f3581b925bd01e6b6
pshu <pishu.spf@antfin.com>
2024-05-11 13:21:29 +0800

    test: ✅ add failure test of mix require in esm for concatenate

---
+++ e2e/fixtures/mako.scope-hoisting.esm_mix_requrie/index.js
@@ -0,0 +1,7 @@
+import {ans, ans2} from "./inner";
+
+it("should have the correct values", function() {
+	expect(ans).toBe(42)
+	expect(ans2).toBe(4242)
+});
+	
---
+++ e2e/fixtures/mako.scope-hoisting.esm_mix_requrie/inner.js
@@ -0,0 +1,5 @@
+import { cjs as unused } from "pkg";
+
+export const ans = 42
+
+export const ans2 = require('pkg').cjs
