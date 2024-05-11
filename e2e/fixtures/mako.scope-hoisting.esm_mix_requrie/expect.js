52061e2e20f40138f94021ea05d57de47df84228
pshu <pishu.spf@antfin.com>
2024-05-11 13:56:31 +0800

    refactor: 🔥 remove cmt

--- e2e/fixtures/mako.scope-hoisting.esm_mix_requrie/expect.js
+++ e2e/fixtures/mako.scope-hoisting.esm_mix_requrie/expect.js
@@ -6,7 +6,6 @@ const {
 const { files } = parseBuildResult(__dirname);
 injectSimpleJest();
 
-// ignored module should not be concatenated, so keep the definition
 expect(files["index.js"]).toContain(moduleDefinitionOf("node_modules/pkg/index.js"));
 
 require("./dist/index.js");
