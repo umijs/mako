5288f1aa9e203a6aa2a779a120556727bba0e734
sorrycc <sorrycc@gmail.com>
2024-05-15 10:22:56 +0800

    feat: add create-mako package

---
+++ packages/create-mako/src/cli.ts
@@ -0,0 +1,43 @@
+import { globSync } from 'glob';
+import path from 'path';
+import fs from 'fs';
+
+async function main() {
+  let templatePath = path.join(__dirname, '../templates/react');
+  let files = globSync('**/*', { cwd: templatePath, nodir: true });
+  let cwd = path.join(process.cwd(), 'mako-project');
+
+  let npmClient = (() => {
+    let script = process.argv[1];
+    if (script.includes('pnpm/')) {
+      return 'pnpm';
+    } else {
+      return 'npm';
+    }
+  })();
+
+  // Copy files
+  for (let file of files) {
+    let source = path.join(templatePath, file);
+    let dest = path.join(cwd, file);
+    console.log(`Creating ${file}`);
+    let destDir = path.dirname(dest);
+    if (!fs.existsSync(destDir)) {
+      fs.mkdirSync(destDir, { recursive: true });
+    }
+    fs.copyFileSync(source, dest);
+  }
+  console.log();
+  console.log('Done, Run following commands to start the project:');
+  console.log();
+  console.log(`  cd ${path.basename(cwd)}`);
+  console.log(`  ${npmClient} install`);
+  console.log(`  ${npmClient} run dev`);
+  console.log();
+  console.log('Happy coding!');
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
