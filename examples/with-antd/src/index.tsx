740e63db6e724af237f89061e9f03e2d20d4382a
pshu <pishu.spf@antfin.com>
2024-05-15 09:50:22 +0800

    revert: ⏪ example code

--- examples/with-antd/src/index.tsx
+++ examples/with-antd/src/index.tsx
@@ -1,7 +1,2 @@
 // import './index-ant-design-icons-only';
-
-chunksIdToUrlMap.cached = 'cached.js';
-
-__mako_require__.ensure('cached').then(() => {
-  require('./index-orig');
-});
+import './index-orig';
