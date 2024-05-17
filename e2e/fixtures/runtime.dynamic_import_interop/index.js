e3512470727d2d8870561358ca8757236058c4ac
pshu <stormslowly@gmail.com>
2024-05-17 16:40:12 +0800

    typo

--- e2e/fixtures/runtime.dynamic_import_interop/index.js
+++ e2e/fixtures/runtime.dynamic_import_interop/index.js
@@ -1,4 +1,4 @@
-it("shoud interop cjs module with default", async () => {
+it("should interop cjs module with default", async () => {
   let cjs = await import("./src/cjs");
 
   expect(cjs).toEqual({ default: { foo: 42 }, foo: 42 });
