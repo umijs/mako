261dc67ac6bd7bfb1e9c15a1a467387a8f110c16
pshu <pishu.spf@antfin.com>
2024-05-19 23:31:35 +0800

    test: ✅ add runtime assert in decorator and target to chrome 40

--- e2e/fixtures/javascript.legacy-decorator/expect.js
+++ e2e/fixtures/javascript.legacy-decorator/expect.js
@@ -1,11 +1,18 @@
 const assert = require("assert");
-const { parseBuildResult, moduleReg } = require("../../../scripts/test-utils");
+const {
+  parseBuildResult,
+  moduleReg,
+  injectSimpleJest,
+} = require("../../../scripts/test-utils");
 const { files } = parseBuildResult(__dirname);
 
 const names = Object.keys(files).join(",");
 const content = files["index.js"];
+injectSimpleJest();
 
 assert(
   content.includes(`Foo = _ts_decorate._([`),
-  "legacy decorator should works"
+  "legacy decorator should works",
 );
+
+require("./dist/index.js");
