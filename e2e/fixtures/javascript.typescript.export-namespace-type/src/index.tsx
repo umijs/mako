b14b3a4eb5c25a29020f092f690d671c3ed90bec
pshu <pishu.spf@antfin.com>
2024-05-27 15:07:05 +0800

    test: ✅ add named export namespace type

--- e2e/fixtures/javascript.typescript.export-namespace-type/src/index.tsx
+++ e2e/fixtures/javascript.typescript.export-namespace-type/src/index.tsx
@@ -1,5 +1,9 @@
-import {bar} from "./type"
+import { bar, FooType } from "./type"
 
 it("cant import value",()=>{
-	expect(bar).toBe('bar')
+  expect(bar).toBe('bar')
 });
+
+it('export namespace type as undefined',()=>{
+  expect(FooType).toBe(undefined)
+})
