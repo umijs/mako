f2e7b751f9b98f977e8d824e2baed2c9a81ebf31
sorrycc <sorrycc@gmail.com>
2024-06-04 17:09:11 +0800

    chore: add examples/react

---
+++ examples/react/src/App.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export function App() {
+  return <h1>App</h1>;
+}
---
+++ examples/react/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+
+throw new Error('This is an error');
+// Promise.reject('这是一段错误信息');
