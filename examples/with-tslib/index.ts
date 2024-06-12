efc91d91f3343782b8615556dabcd32ac13f7f1b
xusd320 <xusd320@gmail.com>
2024-06-12 16:58:08 +0800

    chore: fix biome format

--- examples/with-tslib/index.ts
+++ examples/with-tslib/index.ts
@@ -1,3 +1,3 @@
-import { __assign as t, __spreadArray as e } from 'tslib';
+import { __spreadArray as e, __assign as t } from 'tslib';
 console.log(e([1, 2, 3], [4, 5, 6]));
 console.log(t({ a: 1 }, { b: 2 }));
