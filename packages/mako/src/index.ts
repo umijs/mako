c263ee918bc0d8010b9b65df558e8cf0f9d624b6
Jinbao1001 <nodewebli@gmail.com>
2024-06-13 15:01:57 +0800

    chore: delete test

--- packages/mako/src/index.ts
+++ packages/mako/src/index.ts
@@ -124,7 +124,6 @@ export async function build(params: BuildParams) {
     'forkTSChecker',
     'plugins',
   ]) as BuildParams['config'];
-  console.log('test', params.config.resolve?.alias);
   await binding.build({
     ...params,
     plugins,
