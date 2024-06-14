e2522d90c875804448d8b3672407e65ccdb5a21a
xusd320 <xusd320@gmail.com>
2024-06-14 13:33:14 +0800

    fix: update e2e cases about worker

--- e2e/fixtures/worker.self-spawn/expect.js
+++ e2e/fixtures/worker.self-spawn/expect.js
@@ -2,9 +2,11 @@ const assert = require("assert");
 const { parseBuildResult } = require("../../../scripts/test-utils");
 const { files } = parseBuildResult(__dirname);
 
+const workerDists = Object.entries(files).filter(([fileName, _]) => /^src_workerHelper_ts.*-worker.js$/.test(fileName));
+
 assert(
-  files["src_workerHelper_ts-worker.js"].includes(
-    'new Worker(new URL("src_workerHelper_ts-async.js",'
+  workerDists.length === 1 && workerDists[0][1].includes(
+    `new Worker(new URL("${workerDists[0][0]}"`,
   ),
   "should have self-spawn codes"
 );
