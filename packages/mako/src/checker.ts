2cc618cc387c5d667b2f098fbff5d06e4c4c4a9b
sorrycc <sorrycc@gmail.com>
2024-06-18 18:16:11 +0800

    feat: add pkg checker

---
+++ packages/mako/src/checker.ts
@@ -0,0 +1,43 @@
+import path from 'path';
+import fs from 'fs';
+import semver from 'semver';
+import chalk from 'chalk';
+
+let BLACKLIST_PACKAGES: any = {
+  'pdfjs-dist': {
+    version: "<3.0.0",
+    message: "`pdfjs-dist@2` is not supported, please use `pdfjs-dist@3` or above instead.",
+  },
+  'monaco-editor': {
+    version: "*",
+    message: "`monaco-editor` is not supported, please use `@monaco-editor/react` instead.",
+  },
+};
+
+export function check(root: string) {
+  let pkgPath = path.join(root, 'package.json');
+  if (fs.existsSync(pkgPath)) {
+    let pkg = require(pkgPath);
+    let bPkgs = Object.keys(BLACKLIST_PACKAGES);
+    for (let name of bPkgs) {
+      if (!depExists(pkg, name)) continue;
+      let version = getDepVersion(root, name);
+      if (version && semver.satisfies(version, BLACKLIST_PACKAGES[name].version)) {
+        console.error(chalk.red(`Error: ${BLACKLIST_PACKAGES[name].message}`));
+        process.exit(1);
+      }
+    }
+  }
+}
+
+function depExists(pkg: any, name: string) {
+  return pkg.dependencies[name] || pkg.devDependencies[name];
+}
+
+function getDepVersion(root: string, name: string) {
+  let pkgPath = path.join(root, 'node_modules', name, 'package.json');
+  if (fs.existsSync(pkgPath)) {
+    let pkg = require(pkgPath);
+    return pkg.version;
+  }
+}
