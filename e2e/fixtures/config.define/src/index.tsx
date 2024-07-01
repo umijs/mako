a305044defddb32940d789d483335f9133d20078
xusd320 <xusd320@gmail.com>
2024-07-01 13:07:21 +0800

    fix: define stringified object value will cause panic

--- e2e/fixtures/config.define/src/index.tsx
+++ e2e/fixtures/config.define/src/index.tsx
@@ -1,9 +1,41 @@
 process.env.NODE_ENV;
-console.log(AAA);
-console.log(BBB);
-console.log(CCC);
-console.log(DDD);
-console.log(EEE);
-console.log(FFF);
-console.log(GGG);
-console.log(HHH);
+
+it("defined string value should be right", () => {
+  expect(AAA).toEqual("aaa")
+});
+it("defined number value should be right", () => {
+  expect(BBB).toEqual(1)
+});
+it("defined boolean true value should be right", () => {
+  expect(CCC).toEqual(true)
+});
+it("defined boolean false value should be right", () => {
+  expect(DDD).toEqual(false)
+});
+it("defined null value should be right", () => {
+  expect(EEE).toEqual(null)
+});
+it("defined array value should be right", () => {
+  expect(FFF).toEqual(["a", 1])
+});
+it("defined caculcation value should be right", () => {
+  expect(GGG).toEqual(2)
+});
+it("defined complex object value should be right", () => {
+  expect(HHH).toEqual({
+    "value": "bbb",
+    "ccc": {
+      "d": 1,
+      "e": "2",
+      "c": [
+        1,
+        "2",
+        true
+      ]
+    }
+  })
+});
+it("defined stringified object value should be right", () => {
+  expect(III).toEqual({ v: 1 })
+});
+
