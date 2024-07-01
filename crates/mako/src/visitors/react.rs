b3cb1d8cc470d000dc2df4fd8730f65ea20c3c6f
PeachScript <scdzwyxst@gmail.com>
2024-07-01 18:12:04 +0800

    refactor: correct text

--- crates/mako/src/visitors/react.rs
+++ crates/mako/src/visitors/react.rs
@@ -138,7 +138,7 @@ self.$RefreshSig$ = RefreshRuntime.createSignatureFunctionForTransform;
         code = format!(
             r#"
 if (!(typeof window !== 'undefined' ? window : globalThis).__REACT_DEVTOOLS_GLOBAL_HOOK__) {{
-  console.warn('HMR is not available for React currently! Because React was externalized, please install the React Developers Tools extension to enable React Refresh feature. https://github.com/pmmmwh/react-refresh-webpack-plugin/blob/main/docs/TROUBLESHOOTING.md#externalising-react');
+  console.warn('HMR is not available for React currently! Because React was externalized, please install the React Developer Tools extension to enable React Refresh feature. https://github.com/pmmmwh/react-refresh-webpack-plugin/blob/main/docs/TROUBLESHOOTING.md#externalising-react');
 }}
 {}
         "#,
