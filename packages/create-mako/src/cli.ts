afc2189b3a54daf83a71073f30fe7ccc67965c08
kiner-tang(文辉) <1127031143@qq.com>
2024-07-01 22:48:58 +0800

    feat: optimize code

--- packages/create-mako/src/cli.ts
+++ packages/create-mako/src/cli.ts
@@ -1,22 +1,21 @@
-import fs from 'fs';
-import path from 'path';
-import yargs from 'yargs-parser';
-import { globSync } from 'glob';
-import packageJson from '../package.json';
+import fs from "fs";
+import path from "path";
+import yargs from "yargs-parser";
+import { globSync } from "glob";
 
 const args = yargs(process.argv.slice(2));
 
 async function init(projectName: string) {
-  let templatePath = path.join(__dirname, '../templates/react');
-  let files = globSync('**/*', { cwd: templatePath, nodir: true });
+  let templatePath = path.join(__dirname, "../templates/react");
+  let files = globSync("**/*", { cwd: templatePath, nodir: true });
   let cwd = path.join(process.cwd(), projectName);
 
   let npmClient = (() => {
     let script = process.argv[1];
-    if (script.includes('pnpm/')) {
-      return 'pnpm';
+    if (script.includes("pnpm/")) {
+      return "pnpm";
     } else {
-      return 'npm';
+      return "npm";
     }
   })();
 
@@ -32,31 +31,31 @@ async function init(projectName: string) {
     fs.copyFileSync(source, dest);
   }
   console.log();
-  console.log('Done, Run following commands to start the project:');
+  console.log("Done, Run following commands to start the project:");
   console.log();
   console.log(`  cd ${path.basename(cwd)}`);
   console.log(`  ${npmClient} install`);
   console.log(`  ${npmClient} run dev`);
   console.log(`  # Open http://localhost:3000`);
   console.log();
-  console.log('Happy coding!');
+  console.log("Happy coding!");
 }
 
 async function main() {
   let name = args._[0];
   if (!name) {
-    const inquirer = (await import('inquirer')).default;
+    const inquirer = (await import("inquirer")).default;
     let answers = await inquirer.prompt([
       {
-        type: 'input',
-        name: 'name',
-        message: 'Project name:',
-        default: 'mako-project',
+        type: "input",
+        name: "name",
+        message: "Project name:",
+        default: "mako-project",
       },
     ]);
     name = answers.name;
   }
-  return init(String(name))
+  return init(String(name));
 }
 
 main().catch((err) => {
