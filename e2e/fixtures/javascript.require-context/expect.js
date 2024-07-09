9e3c513587c106d7ee9e5e55ef5fdc90464b1ab6
pshu <pishu.spf@antfin.com>
2024-07-09 13:58:21 +0800

    test: ✅ add lazy case

--- e2e/fixtures/javascript.require-context/expect.js
+++ e2e/fixtures/javascript.require-context/expect.js
@@ -1,5 +1,13 @@
-const { injectSimpleJest } = require("../../../scripts/test-utils");
+const {
+  injectSimpleJest,
+  parseBuildResult,
+} = require("../../../scripts/test-utils");
+const { files } = parseBuildResult(__dirname);
 
 injectSimpleJest();
 
 require("./dist/index");
+
+expect(
+  Object.keys(files).filter((f) => f.endsWith(".js")).length,
+).toBeGreaterThan(2);
