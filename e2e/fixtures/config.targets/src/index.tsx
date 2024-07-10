ea2376bcfa1f8c2f90685f91994076ed820f791a
Jinbao1001 <nodewebli@gmail.com>
2024-07-10 15:58:39 +0800

    fix: should inspect  reserved_words after preset_env (#1367)
    * fix: reserved_words
    
    * chore: add e2e runtime for reserved_words

--- e2e/fixtures/config.targets/src/index.tsx
+++ e2e/fixtures/config.targets/src/index.tsx
@@ -1,2 +1,14 @@
 const f = () => {};
 console.log(f);
+
+
+const a =  {
+  default : class {
+
+  }
+}
+it('class with obj.default should work ',()=>{
+  const b = new a.default()
+  console.log(b)
+})
+
