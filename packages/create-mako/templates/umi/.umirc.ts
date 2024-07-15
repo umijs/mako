3703fabe6aa7b66b592d42da26f3bfa689abf7e3
kiner-tang <1127031143@qq.com>
2024-07-15 09:13:04 +0800

    feat: add umi template (#1408)

---
+++ packages/create-mako/templates/umi/.umirc.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  routes: [
+    { path: '/', component: 'index' },
+    { path: '/docs', component: 'docs' },
+  ],
+  npmClient: 'pnpm',
+  mako: {},
+});
