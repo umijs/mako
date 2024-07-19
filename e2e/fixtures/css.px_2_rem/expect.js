8a9c73097cd48d18788c60acc1c7a3817d82bfcf
pshu <pishu.spf@antfin.com>
2024-07-19 22:28:09 +0800

    test: ✅ better assert

--- e2e/fixtures/css.px_2_rem/expect.js
+++ e2e/fixtures/css.px_2_rem/expect.js
@@ -3,9 +3,9 @@ const assert = require("assert");
 const { parseBuildResult, trim } = require("../../../scripts/test-utils");
 const { files } = parseBuildResult(__dirname);
 
-assert(
-  files["index.css"].trim() ===
-    `
+assert.equal(
+  files["index.css"].trim(),
+  `
 @media (min-width: 500px) {
   .container {
     padding-top: 0.8rem;
