7ab4b9b2bd9aca2a6e3bb08449c1e30c3af3e222
gin-lsl <gin_lsl@outlook.com>
2024-07-31 23:58:28 +0800

    test case text

--- e2e/fixtures.umi/config.less.globalVars/expect.js
+++ e2e/fixtures.umi/config.less.globalVars/expect.js
@@ -5,4 +5,4 @@ const { files } = parseBuildResult(__dirname);
 let content = files["pages_index_tsx-async.css"];
 content = content.replace(/\s/g, "");
 
-assert(content.includes(`color:red;`), "should prefer less.modifyVars than config.theme");
+assert(content.includes(`color:red;`), "should available less.globalVars");
