aac8601c92b5835176b24452bb3f8af05a241b1c
pshu <pishu.spf@antfin.com>
2024-08-07 21:13:33 +0800

    test: ✅ debug test case

--- e2e/fixtures.umi/config.less.plugins/expect.js
+++ e2e/fixtures.umi/config.less.plugins/expect.js
@@ -5,4 +5,5 @@ const { files } = parseBuildResult(__dirname);
 let content = files["pages_index_tsx-async.css"];
 content = content.replace(/\s/g, "");
 
+console.log(content);
 assert(content.includes(`height:1.1px`), "less-plugin-clean-css should work");
