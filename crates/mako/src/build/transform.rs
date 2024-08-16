dda60a2ed4ec3e57e4647cfd592a86ed3fa7df67
野声 <artin@cat.ms>
2024-08-16 10:16:13 +0800

    fix: support require css modules (#1501)
    * fix: support require css modules
    
    * fix: remove auto css module option
    
    * chore: revert example change
    
    * chore: update comments

--- crates/mako/src/build/transform.rs
+++ crates/mako/src/build/transform.rs
@@ -136,6 +136,7 @@ impl Transform {
                     )));
                     visitors.push(Box::new(VirtualCSSModules {
                         auto_css_modules: context.config.auto_css_modules,
+                        unresolved_mark,
                     }));
                     // TODO: move ContextModuleVisitor out of plugin
                     visitors.push(Box::new(ContextModuleVisitor { unresolved_mark }));
