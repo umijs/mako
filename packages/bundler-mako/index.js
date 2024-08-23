193b7dd884f680cddde4664c5363cbb7201f29da
xusd320 <xusd320@gmail.com>
2024-08-23 16:00:47 +0800

    fix: should not alias define XXX to process.env.XXX (#1526)
    * fix: should not assign define XXX to process.env.XXX
    
    * fix: process.env.SOCKER_SERVER

--- packages/bundler-mako/index.js
+++ packages/bundler-mako/index.js
@@ -509,21 +509,14 @@ async function getMakoConfig(opts) {
   const define = {};
   if (opts.config.define) {
     for (const key of Object.keys(opts.config.define)) {
-      // mako 的 define 会先去判断 process.env.xxx，再去判断 xxx
-      // 这里传 process.env.xxx 反而不会生效
-      // TODO: 待 mako 改成和 umi/webpack 的方式一致之后，可以把这段去掉
-      if (key.startsWith('process.env.')) {
-        define[key.replace(/^process\.env\./, '')] = normalizeDefineValue(
-          opts.config.define[key],
-        );
-      } else {
-        define[key] = normalizeDefineValue(opts.config.define[key]);
-      }
+      define[key] = normalizeDefineValue(opts.config.define[key]);
     }
   }
 
   if (process.env.SOCKET_SERVER) {
-    define.SOCKET_SERVER = normalizeDefineValue(process.env.SOCKET_SERVER);
+    define['process.env.SOCKET_SERVER'] = normalizeDefineValue(
+      process.env.SOCKET_SERVER,
+    );
   }
 
   let minify = jsMinifier === 'none' ? false : true;
