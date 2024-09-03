025591a6eb891b734fcafc2d15ae5ef06518f326
pshu <pishu.spf@antfin.com>
2024-09-03 10:18:41 +0800

    Revert "refactor: Unify the static server in bundler-mako and devServer (#1468)" (#1556)
    This reverts commit 895d2943f2536aa56d3ef226fe5fe2a64d08c077.

--- packages/bundler-mako/index.js
+++ packages/bundler-mako/index.js
@@ -111,17 +111,27 @@ exports.dev = async function (opts) {
     logLevel: 'silent',
   });
   app.use('/__/hmr-ws', wsProxy);
-  app.use((req, res, next) => {
-    if (req.method !== 'GET' && req.method !== 'HEAD') {
-      return next();
+
+  const outputPath = path.resolve(opts.cwd, opts.config.outputPath || 'dist');
+
+  function processReqURL(publicPath, reqURL) {
+    if (!publicPath.startsWith('/')) {
+      publicPath = '/' + publicPath;
     }
-    if (req.accepts().join('').includes('html')) {
-      return next();
-    }
-    wsProxy(req, res, () => {
+    return reqURL.startsWith(publicPath)
+      ? reqURL.slice(publicPath.length - 1)
+      : reqURL;
+  }
+
+  if (opts.config.publicPath) {
+    app.use((req, _res, next) => {
+      req.url = processReqURL(opts.config.publicPath, req.url);
       next();
     });
-  });
+  }
+
+  // serve dist files
+  app.use(express.static(outputPath));
 
   if (process.env.SSU === 'true') {
     // for ssu cache chunks
