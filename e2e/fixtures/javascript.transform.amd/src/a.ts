cac44f0c28fa36fb2c18f78d1324285684bf086e
Jinbao1001 <nodewebli@gmail.com>
2024-10-15 22:07:43 +0800

    fix: umd should be import as cjs

---
+++ e2e/fixtures/javascript.transform.amd/src/a.ts
@@ -0,0 +1,13 @@
+// @ts-nocheck
+(function (root, definition) {
+    "use strict";
+    if (typeof define === 'function' && define.amd) {
+        define(definition);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = definition();
+    } else {
+        root.log = definition();
+    }
+}(this, function () {
+  return 1
+}))
