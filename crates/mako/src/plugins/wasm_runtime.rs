8daa5bb914734e7f4e57c4147f9f99a829f09c93
御风 <18012261618@126.com>
2024-11-27 15:21:14 +0800

    fix: 修复load .wasm文件对importObject的处理

--- crates/mako/src/plugins/wasm_runtime.rs
+++ crates/mako/src/plugins/wasm_runtime.rs
@@ -1,9 +1,14 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Read;
 use std::sync::Arc;
 
 use anyhow;
+use wasmparser::{Import, Parser, Payload};
 
+use crate::ast::file::{Content, JsContent};
 use crate::compiler::Context;
-use crate::plugin::Plugin;
+use crate::plugin::{Plugin, PluginLoadParam};
 
 pub struct WasmRuntimePlugin {}
 
@@ -26,5 +31,89 @@ impl Plugin for WasmRuntimePlugin {
         } else {
             Ok(vec![])
         }
+    }
+
+    fn load(
+        &self,
+        param: &PluginLoadParam,
+        _context: &Arc<Context>,
+    ) -> anyhow::Result<Option<Content>> {
+        let file = param.file;
+        if file.path.to_string_lossy().ends_with(".wasm") {
+            let final_file_name = format!(
+                "{}.{}.{}",
+                file.get_file_stem(),
+                file.get_content_hash()?,
+                file.extname
+            );
+            let origin_path = file.pathname.to_string_lossy().to_string();
+            _context.emit_assets(origin_path, final_file_name.clone());
+
+            let mut buffer = Vec::new();
+            File::open(file.path.as_path())?.read_to_end(&mut buffer)?;
+            // Parse wasm file to get imports
+            let mut import_objs_map: HashMap<&str, Vec<String>> = HashMap::new();
+            for payload in Parser::new(0).parse_all(&buffer) {
+                if let Ok(Payload::ImportSection(imports)) = payload {
+                    for import in imports {
+                        match import {
+                            Ok(Import { module, name, ty }) => {
+                                if let Some(import_obj) = import_objs_map.get_mut(module) {
+                                    import_obj.push(name.to_string());
+                                } else {
+                                    import_objs_map.insert(module, vec![name.to_string()]);
+                                }
+                            }
+                            Err(_) => {
+                                println!("import error");
+                            }
+                        }
+                    }
+                }
+            }
+
+            let mut js_imports_str = String::new();
+            let mut import_objs_str = String::new();
+
+            for (index, (key, value)) in import_objs_map.iter().enumerate() {
+                js_imports_str.push_str(&format!(
+                    "import * as module{module_idx} from \"{module}\";\n",
+                    module_idx = index,
+                    module = key
+                ));
+
+                import_objs_str.push_str(&format!(
+                    "\"{module}\": {{ {names} }}",
+                    module = key,
+                    names = value
+                        .iter()
+                        .map(|name| format!("{}: module{}.{}", name, index, name))
+                        .collect::<Vec<String>>()
+                        .join(", ")
+                ));
+            }
+
+            let mut content = String::new();
+            content.push_str(&js_imports_str);
+
+            if import_objs_str.is_empty() {
+                content.push_str(&format!(
+                    "module.exports = require._interopreRequireWasm(exports, \"{}\")",
+                    final_file_name
+                ));
+            } else {
+                content.push_str(&format!(
+                    "module.exports = require._interopreRequireWasm(exports, \"{}\", {{{}}})",
+                    final_file_name, import_objs_str
+                ));
+            }
+
+            return Ok(Some(Content::Js(JsContent {
+                content,
+                ..Default::default()
+            })));
+        }
+
+        Ok(None)
     }
 }
---
+++ examples/import-resources/minus-wasm-pack/index_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function greet2(a: number, b: number): void;
+export function minus(a: number, b: number): number;
+export function greet(): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+): number;
