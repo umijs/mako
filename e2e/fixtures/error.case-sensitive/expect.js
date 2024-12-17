8b0829f8661acf67961477f0c14d700213e81952
shikuan.sk <shikuan.sk@antgroup.com>
2024-12-17 16:10:49 +0800

    fix: 非mac环境断言

--- e2e/fixtures/error.case-sensitive/expect.js
+++ e2e/fixtures/error.case-sensitive/expect.js
@@ -1,8 +1,14 @@
 const assert = require("assert");
+const os = require("os");
+const { files } = parseBuildResult(__dirname);
 
 module.exports = (err) => {
-  assert(
-    err.stderr.includes(`/Assets/umi-logo.png does not match the corresponding path on disk [assets]`),
-    "should throw error"
-  );
+  if (os.platform() === 'darwin') {
+    assert(
+      err.stderr.includes(`/Assets/umi-logo.png does not match the corresponding path on disk [assets]`),
+      "should throw error"
+    );
+  } else {
+    assert("index.js" in files, "should have file: index.js");
+  }
 };
