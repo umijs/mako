79e7855a1776bbff235481bfabc2be9befc8b873
xusd320 <xusd320@gmail.com>
2025-03-12 17:17:44 +0800

    feat: bundler prototype based on turbopack

--- crates/bundler/src/lib.rs
+++ crates/bundler/src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![feature(future_join)]
+#![feature(min_specialization)]
+#![feature(arbitrary_self_types)]
+#![feature(arbitrary_self_types_pointers)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod arguments;
+pub mod build;
+pub mod contexts;
+pub mod dev;
+pub mod env;
+pub mod issue;
+pub mod runtime_entry;
+pub mod source_context;
+pub mod util;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn register() {
+    turbopack::register();
+    turbopack_nodejs::register();
+    turbopack_browser::register();
+    turbopack_ecmascript_plugins::register();
+    turbopack_resolve::register();
+    include!(concat!(env!("OUT_DIR"), "/register.rs"));
 }
---
+++ crates/bundler/src/main.rs
@@ -0,0 +1,89 @@
+#![feature(future_join)]
+#![feature(min_specialization)]
+
+use std::path::Path;
+
+use anyhow::{Context, Result};
+use clap::Parser;
+use tracing::subscriber;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, Registry};
+use turbo_tasks_malloc::TurboMalloc;
+use turbopack_trace_utils::{
+    exit::ExitHandler,
+    filter_layer::FilterLayer,
+    raw_trace::RawTraceLayer,
+    trace_writer::{self, TraceWriter},
+    tracing_presets::{
+        TRACING_OVERVIEW_TARGETS, TRACING_TURBOPACK_TARGETS, TRACING_TURBO_TASKS_TARGETS,
+    },
+};
+
+use bundler::{arguments::Arguments, register};
+
+#[global_allocator]
+static ALLOC: TurboMalloc = TurboMalloc;
+
+fn main() {
+    let args = Arguments::parse();
+
+    tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .on_thread_stop(|| {
+            TurboMalloc::thread_stop();
+        })
+        .disable_lifo_slot()
+        .build()
+        .unwrap()
+        .block_on(main_inner(args))
+        .unwrap();
+}
+
+async fn main_inner(args: Arguments) -> Result<()> {
+    let exit_handler = ExitHandler::listen();
+
+    let trace = std::env::var("TURBOPACK_TRACING").ok();
+    if let Some(mut trace) = trace.filter(|v| !v.is_empty()) {
+        match trace.as_str() {
+            "overview" => {
+                trace = TRACING_OVERVIEW_TARGETS.join(",");
+            }
+            "turbopack" => {
+                trace = TRACING_TURBOPACK_TARGETS.join(",");
+            }
+            "turbo-tasks" => {
+                trace = TRACING_TURBO_TASKS_TARGETS.join(",");
+            }
+            _ => {}
+        }
+
+        let subscriber = Registry::default();
+
+        let subscriber = subscriber.with(FilterLayer::try_new(&trace).unwrap());
+
+        let internal_dir = args
+            .dir()
+            .unwrap_or_else(|| Path::new("."))
+            .join(".turbopack");
+
+        std::fs::create_dir_all(&internal_dir)
+            .context("Unable to create .turbopack directory")
+            .unwrap();
+        let trace_file = internal_dir.join("trace.log");
+        let trace_writer = std::fs::File::create(trace_file).unwrap();
+        let (trace_writer, guard) = TraceWriter::new(trace_writer);
+        let subscriber = subscriber.with(RawTraceLayer::new(trace_writer));
+
+        exit_handler.on_exit(async move {
+            tokio::task::spawn_blocking(|| drop(guard)).await.unwrap();
+        });
+
+        subscriber.init();
+    }
+
+    register();
+
+    match args {
+        Arguments::Build(args) => bundler::build::build(&args).await,
+        Arguments::Dev(args) => bundler::dev::start_server(&args).await,
+    }
+}
