ca1d73719a7d5108dedfa0646a3042f3e8775d9b
御风 <18012261618@126.com>
2025-03-17 02:29:43 +0800

    fix: 兼容url()

--- packages/mako/src/plugins/less/less-import-plugin.ts
+++ packages/mako/src/plugins/less/less-import-plugin.ts
@@ -8,7 +8,18 @@ export default class LessImportPlugin {
       run(root, visitArgs) {
         return new less.visitors.Visitor({
           visitImport(node, visitArgs) {
-            if (node.path.value.endsWith('.less')) {
+            let pathValue;
+
+            // 只处理 @import '*.less' | url('*.less')
+            if (node.path instanceof less.tree.Quoted) {
+              pathValue = node.path.value;
+            } else if (node.path instanceof less.tree.URL) {
+              if (node.path.value instanceof less.tree.Quoted) {
+                pathValue = node.path.value.value;
+              }
+            }
+
+            if (pathValue?.endsWith('.less')) {
               node.options.value = 'css';
               node.options.less = false;
             }
