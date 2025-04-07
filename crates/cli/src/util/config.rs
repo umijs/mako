13331d3eed8ba6219b48e665b14f6fcc553dec65
elrrrrrrr <elrrrrrrr@gmail.com>
2025-04-07 11:16:44 +0800

    refact(cli): use utoo-core to load config (#1794)

--- crates/cli/src/util/config.rs
+++ crates/cli/src/util/config.rs
@@ -1,14 +1,34 @@
 use std::sync::OnceLock;
+use utoo_core::config::Config;
+
+use super::logger::log_verbose;
 
 static REGISTRY: OnceLock<String> = OnceLock::new();
 
-pub fn set_registry(registry: &str) {
-    let _ = REGISTRY.set(registry.to_string());
+pub fn set_registry(registry: Option<String>) {
+    if let Some(registry) = registry {
+        if !registry.trim().is_empty() {
+            log_verbose(&format!("set registry: {}", registry));
+            let _ = REGISTRY.set(registry);
+        }
+    }
 }
 
 pub fn get_registry() -> &'static str {
-    REGISTRY
-        .get()
-        .map(|s| s.as_str())
-        .unwrap_or("https://registry.npmmirror.com")
+    if let Some(registry) = REGISTRY.get() {
+        if !registry.is_empty() {
+            return registry.as_str();
+        }
+    }
+
+    // load from utoo-core config
+    if let Ok(config) = Config::load(false) {
+        if let Ok(Some(registry)) = config.get("registry") {
+            let _ = REGISTRY.set(registry);
+            return REGISTRY.get().unwrap().as_str();
+        }
+    }
+
+    // default to npmmirror
+    "https://registry.npmmirror.com"
 }
