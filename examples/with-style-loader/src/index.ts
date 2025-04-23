405ca0fe08c56764ecaaac6c5f172e0c32dcf840
xusd320 <xusd320@gmail.com>
2025-04-23 10:00:22 +0800

    fix(bundler): style-loader with css module

--- examples/with-style-loader/src/index.ts
+++ examples/with-style-loader/src/index.ts
@@ -1,4 +1,12 @@
 import "./index.less";
 import "./index.sass";
-document.body.innerHTML = "<div>Style loader example</div>";
 
+// @ts-ignore
+import styleLess from "./style.module.less";
+// @ts-ignore
+import styleSass from "./style.module.sass";
+
+document.body.innerHTML = `<div>Style loader example
+  <div class=${JSON.stringify(styleLess.less)}>Less Module</div>
+  <div class=${JSON.stringify(styleSass.sass)}>Sass Module</div>
+</div>`;
