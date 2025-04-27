fbb1280b358de31cb001ec14fd00ff3a5d96c083
xusd320 <xusd320@gmail.com>
2025-04-28 00:33:11 +0800

    feat(bundler): support filename template with contenthash

--- crates/bundler-core/src/library/ecmascript/mod.rs
+++ crates/bundler-core/src/library/ecmascript/mod.rs
@@ -1,6 +1,3 @@
 pub(crate) mod chunk;
-pub(crate) mod content;
-pub(crate) mod version;
 
-pub use chunk::EcmascriptLibraryChunk;
-pub use content::EcmascriptLibraryChunkContent;
+pub use chunk::EcmascriptLibraryEvaluateChunk;
