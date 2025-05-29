78e7bd01d614ecf4c686b42821dd667ce6f1055c
xusd320 <xusd320@gmail.com>
2025-05-29 14:24:57 +0800

    chore(bundler): add example multiple-entries-heavy

---
+++ examples/multiple-entries-heavy/src/entry-7.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import reactDom from 'react-dom';
+import * as antd from 'antd';
+import * as three from 'three';
+import * as lodash from 'lodash';
+import * as axios from 'axios';
+
+window.React = react;
+window.ReactDom = reactDom;
+window.Antd = antd;
+window.Three = three;
+window.Lodash = lodash;
+window.Axios = axios;
