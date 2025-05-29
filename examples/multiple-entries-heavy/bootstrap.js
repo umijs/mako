bd07b975a60478be4b3ea448b71c407648acc7d6
xusd320 <xusd320@gmail.com>
2025-05-29 16:02:32 +0800

    chore(bundler): add example multiple-entries-heavy (#1918)

---
+++ examples/multiple-entries-heavy/bootstrap.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const path = require("path");
+
+const total = 20;
+const content = `import react from 'react';
+import reactDom from 'react-dom';
+import * as antd from 'antd';
+import * as three from 'three';
+import * as lodash from 'lodash';
+import * as axios from 'axios';
+
+window.React = react;
+window.ReactDom = reactDom;
+window.Antd = antd;
+window.Three = three;
+window.Lodash = lodash;
+window.Axios = axios;
+`;
+
+const projectOptions = {
+  rootPath: "../../",
+  projectPath: "./",
+  config: {
+    entry: [],
+    output: {
+      path: "./dist",
+    },
+  },
+};
+
+for (let i = 0; i < total; i++) {
+  const name = `entry-${i}`;
+  const filename = `${name}.js`;
+
+  fs.writeFileSync(path.join(__dirname, "./src", filename), content);
+
+  projectOptions.config.entry.push({
+    import: `./src/entry-${i}.js`,
+  });
+
+  fs.writeFileSync(
+    path.join(__dirname, "project_options.json"),
+    JSON.stringify(projectOptions, null, 2),
+  );
+}
