f19b07573a2916bc5fde0d6861112a7cfdc2ecca
xusd320 <xusd320@gmail.com>
2025-06-25 18:20:24 +0800

    perf(pack): improve stats generateion

--- crates/pack-api/src/webpack_stats.rs
+++ crates/pack-api/src/webpack_stats.rs
@@ -1,4 +1,7 @@
-use anyhow::Result;
+use std::sync::Arc;
+
+use anyhow::{Error, Result};
+use parking_lot::Mutex;
 use qstring::QString;
 use serde::Serialize;
 use turbo_rcstr::RcStr;
@@ -14,127 +17,156 @@ pub async fn generate_webpack_stats<'a, I>(output_assets: I) -> Result<WebpackSt
 where
     I: IntoIterator<Item = &'a ResolvedVc<Box<dyn OutputAsset>>>,
 {
-    let mut assets = vec![];
-    let mut chunks = vec![];
-    let mut chunk_items: FxIndexMap<Vc<Box<dyn ChunkItem>>, FxIndexSet<RcStr>> =
-        FxIndexMap::default();
-    let mut modules = vec![];
-    let mut entrypoints = FxIndexMap::default();
-    for asset in output_assets {
-        let asset_len = asset.size_bytes().await?.unwrap_or_default();
+    let assets: Arc<Mutex<Vec<WebpackStatsAsset>>> = Arc::new(Mutex::new(vec![]));
+    let chunks: Arc<Mutex<Vec<WebpackStatsChunk>>> = Arc::new(Mutex::new(vec![]));
+    let chunk_items: Arc<Mutex<FxIndexMap<Vc<Box<dyn ChunkItem>>, FxIndexSet<RcStr>>>> =
+        Arc::new(Mutex::new(FxIndexMap::default()));
+    let modules: Arc<Mutex<Vec<WebpackStatsModule>>> = Arc::new(Mutex::new(vec![]));
+    let entrypoints: Arc<Mutex<FxIndexMap<RcStr, WebpackStatsEntrypoint>>> =
+        Arc::new(Mutex::new(FxIndexMap::default()));
 
-        if let Some(chunk) = ResolvedVc::try_downcast_type::<EcmascriptBrowserEvaluateChunk>(*asset)
-        {
-            let entry_path = chunk.path().await?.path.clone();
-            chunks.push(WebpackStatsChunk {
-                size: asset_len,
-                files: vec![entry_path.clone()],
-                id: entry_path.clone(),
-                ..Default::default()
-            });
+    output_assets
+        .into_iter()
+        .map(|asset| {
+            let chunks = chunks.clone();
+            let chunk_items = chunk_items.clone();
+            let entrypoints = entrypoints.clone();
+            let assets = assets.clone();
+            async move {
+                let asset_len = asset.size_bytes().await?.unwrap_or_default();
 
-            chunk
-                .evaluatable_assets()
-                .await?
-                .iter()
-                .for_each(|evaluatable_asset| {
-                    let item = evaluatable_asset
-                        .as_chunk_item(chunk.module_graph(), chunk.chunking_context());
-                    chunk_items
-                        .entry(item)
-                        .or_default()
-                        .insert(entry_path.clone());
+                if let Some(chunk) =
+                    ResolvedVc::try_downcast_type::<EcmascriptBrowserEvaluateChunk>(*asset)
+                {
+                    let entry_path = chunk.path().await?.path.clone();
+                    let mut chunks = chunks.lock();
+                    chunks.push(WebpackStatsChunk {
+                        size: asset_len,
+                        files: vec![entry_path.clone()],
+                        id: entry_path.clone(),
+                        ..Default::default()
+                    });
+
+                    chunk
+                        .evaluatable_assets()
+                        .await?
+                        .iter()
+                        .for_each(|evaluatable_asset| {
+                            let item = evaluatable_asset
+                                .as_chunk_item(chunk.module_graph(), chunk.chunking_context());
+                            let mut chunk_items = chunk_items.lock();
+                            chunk_items
+                                .entry(item)
+                                .or_default()
+                                .insert(entry_path.clone());
+                        });
+
+                    let entry_referenced_assets = (*chunk).chunks_data().await?;
+                    let mut entry_chunks = entry_referenced_assets
+                        .iter()
+                        .map(async |asset| Ok(asset.await?.path.as_str().into()))
+                        .try_join()
+                        .await?;
+                    entry_chunks.push(entry_path.clone());
+
+                    let mut entry_assets = entry_referenced_assets
+                        .iter()
+                        .map(|asset| async move {
+                            Ok(WebpackStatsEntrypointAssets {
+                                name: asset.await?.path.as_str().into(),
+                            })
+                        })
+                        .try_join()
+                        .await?;
+                    entry_assets.push(WebpackStatsEntrypointAssets {
+                        name: entry_path.clone(),
+                    });
+
+                    let entry_name: RcStr = QString::from((*chunk).ident().query().await?.as_str())
+                        .get("name")
+                        .unwrap_or(remove_extension_from_str(entry_path.as_str()))
+                        .into();
+
+                    let mut entrypoints = entrypoints.lock();
+                    entrypoints.insert(
+                        entry_name.clone(),
+                        WebpackStatsEntrypoint {
+                            name: entry_name.clone(),
+                            chunks: entry_chunks,
+                            assets: entry_assets,
+                        },
+                    );
+                }
+
+                if let Some(chunk) = ResolvedVc::try_downcast_type::<EcmascriptBrowserChunk>(*asset)
+                {
+                    let chunk_ident = &chunk.path().await?.path;
+                    let mut chunks = chunks.lock();
+                    chunks.push(WebpackStatsChunk {
+                        size: asset_len,
+                        files: vec![chunk_ident.clone()],
+                        id: chunk_ident.clone(),
+                        ..Default::default()
+                    });
+
+                    for item in chunk.chunk().chunk_items().await? {
+                        let mut chunk_items = chunk_items.lock();
+                        chunk_items
+                            .entry(**item)
+                            .or_default()
+                            .insert(chunk_ident.clone());
+                    }
+                }
+
+                if let Some(chunk) = ResolvedVc::try_downcast_type::<CssChunk>(*asset) {
+                    let chunk_ident = &chunk.path().await?.path;
+                    let mut chunks = chunks.lock();
+                    chunks.push(WebpackStatsChunk {
+                        size: asset_len,
+                        files: vec![chunk_ident.clone()],
+                        id: chunk_ident.clone(),
+                        ..Default::default()
+                    });
+                }
+
+                let path = &asset.path().await?.path;
+                let mut assets = assets.lock();
+                assets.push(WebpackStatsAsset {
+                    ty: "asset".into(),
+                    name: path.clone(),
+                    chunks: vec![path.clone()],
+                    size: asset_len,
+                    ..Default::default()
                 });
-
-            let entry_referenced_assets = (*chunk).chunks_data().await?;
-            let mut entry_chunks = entry_referenced_assets
-                .iter()
-                .map(async |asset| Ok(asset.await?.path.as_str().into()))
-                .try_join()
-                .await?;
-            entry_chunks.push(entry_path.clone());
-
-            let mut entry_assets = entry_referenced_assets
-                .iter()
-                .map(|asset| async move {
-                    Ok(WebpackStatsEntrypointAssets {
-                        name: asset.await?.path.as_str().into(),
-                    })
-                })
-                .try_join()
-                .await?;
-            entry_assets.push(WebpackStatsEntrypointAssets {
-                name: entry_path.clone(),
-            });
-
-            let entry_name: RcStr = QString::from((*chunk).ident().query().await?.as_str())
-                .get("name")
-                .unwrap_or(remove_extension_from_str(entry_path.as_str()))
-                .into();
-
-            entrypoints.insert(
-                entry_name.clone(),
-                WebpackStatsEntrypoint {
-                    name: entry_name.clone(),
-                    chunks: entry_chunks,
-                    assets: entry_assets,
-                },
-            );
-        }
-
-        if let Some(chunk) = ResolvedVc::try_downcast_type::<EcmascriptBrowserChunk>(*asset) {
-            let chunk_ident = &chunk.path().await?.path;
-            chunks.push(WebpackStatsChunk {
-                size: asset_len,
-                files: vec![chunk_ident.clone()],
-                id: chunk_ident.clone(),
-                ..Default::default()
-            });
-
-            for item in chunk.chunk().chunk_items().await? {
-                chunk_items
-                    .entry(**item)
-                    .or_default()
-                    .insert(chunk_ident.clone());
+                Ok::<(), Error>(())
             }
-        }
+        })
+        .try_join()
+        .await?;
 
-        if let Some(chunk) = ResolvedVc::try_downcast_type::<CssChunk>(*asset) {
-            let chunk_ident = &chunk.path().await?.path;
-            chunks.push(WebpackStatsChunk {
-                size: asset_len,
-                files: vec![chunk_ident.clone()],
-                id: chunk_ident.clone(),
-                ..Default::default()
+    let chunk_items = chunk_items.lock();
+    chunk_items
+        .iter()
+        .map(|(chunk_item, chunks)| async {
+            let size = *chunk_item.content_ident().path().read().len().await?;
+            let path = chunk_item.asset_ident().path().await?.path.clone();
+            let mut modules = modules.lock();
+            modules.push(WebpackStatsModule {
+                name: path.clone(),
+                id: path.clone(),
+                chunks: chunks.iter().cloned().collect(),
+                size,
             });
-        }
-
-        let path = &asset.path().await?.path;
-        assets.push(WebpackStatsAsset {
-            ty: "asset".into(),
-            name: path.clone(),
-            chunks: vec![path.clone()],
-            size: asset_len,
-            ..Default::default()
-        });
-    }
-
-    for (chunk_item, chunks) in chunk_items {
-        let size = *chunk_item.content_ident().path().read().len().await?;
-        let path = chunk_item.asset_ident().path().await?.path.clone();
-        modules.push(WebpackStatsModule {
-            name: path.clone(),
-            id: path.clone(),
-            chunks: chunks.into_iter().collect(),
-            size,
-        });
-    }
+            Ok(())
+        })
+        .try_join()
+        .await?;
 
     Ok(WebpackStats {
-        assets,
-        entrypoints,
-        chunks,
-        modules,
+        assets: Arc::into_inner(assets).unwrap().into_inner(),
+        entrypoints: Arc::into_inner(entrypoints).unwrap().into_inner(),
+        chunks: Arc::into_inner(chunks).unwrap().into_inner(),
+        modules: Arc::into_inner(modules).unwrap().into_inner(),
     })
 }
 
