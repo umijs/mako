a8ded45254bcdf9011bf1254a9f31ebfe4ee7184
xusd320 <xusd320@gmail.com>
2025-06-30 10:41:36 +0800

    fix(pack): fix define env (#2002)

--- examples/webpack-compat/pack.ts
+++ examples/webpack-compat/pack.ts
@@ -12,7 +12,7 @@ const webpackConfig: utoopack.WebpackConfig = {
     new webpack.DefinePlugin({
       VERSION: JSON.stringify("5fa3b9"),
       BROWSER_SUPPORTS_HTML5: true,
-      TWO: "1+1",
+      TWO: "1",
       "typeof window": JSON.stringify("object"),
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
     }),
